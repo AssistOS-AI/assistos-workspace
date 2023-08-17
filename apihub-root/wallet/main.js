bf95599770f5c14215fd01eaabe8c17a15ad405d
alinaduca <alina_duca@yahoo.com>
2023-08-17 14:50:15 +0300

    Adding personalities page and changing the sidebar.

--- apihub-root/wallet/main.js
+++ apihub-root/wallet/main.js
@@ -1,4 +1,5 @@
 import { llmsPage, showActionBox } from "./presenters/llms-page.js";
+import { personalitiesPage } from "./presenters/personalities-page.js";
 import { closeModal, showModal } from "./scripts/WebSkel/utils/modal-utils.js";
 import WebSkel from "./scripts/WebSkel/webSkel.js";
 
@@ -43,17 +44,21 @@ window.webSkel = new WebSkel();
 webSkel.setDomElementForPages(document.querySelector("#page-content"));
 
 webSkel.registerPresenter("llms-page", llmsPage);
-webSkel.registerAction("showAddLLMModal", async (...params) => {
-    await showModal(webSkel._documentElement, "add-llm-modal", {});
-})
+webSkel.registerPresenter("personalities-page", personalitiesPage);
+// webSkel.registerAction("showAddLLMModal", async (...params) => {
+//     await showModal(webSkel._documentElement, "add-llm-modal", {});
+// })
 
 webSkel.registerAction("closeModal", async (modal, _param) => {
     closeModal(modal);
 });
 
 webSkel.registerAction("showAddLLMModal", async (...params) => {
-    console.log("Do something");
     await showModal(document.querySelector("body"), "add-llm-modal", {});
+})
+
+webSkel.registerAction("showAddPersonalityModal", async (...params) => {
+    await showModal(document.querySelector("body"), "add-personality-modal", {});
 })
 
 webSkel.registerAction("changePage", async (_target, pageId) => {
@@ -67,7 +72,9 @@ webSkel.registerAction("showActionBox", async (_target, primaryKey) => {
 /* Modal components defined here */
 webSkel.defineComponent("add-llm-modal", "../components/add-llm-modal/add-llm-modal.html");
 webSkel.defineComponent("llm-item-renderer","../components/llm-item-renderer/llm-item-renderer.html");
+webSkel.defineComponent("personality-item-renderer","../components/personality-item-renderer/personality-item-renderer.html");
 webSkel.defineComponent("llms-page", "../pages/llms-page/llms-page.html");
+webSkel.defineComponent("personalities-page", "../pages/personalities-page/personalities-page.html");
 
 (async ()=>{
     await initWallet();
---
+++ apihub-root/wallet/presenters/personalities-page.js
@@ -0,0 +1,56 @@
+import { Company } from "../core/company.js";
+
+export class personalitiesPage {
+    constructor() {
+        this.title = "Personalities";
+        this.shortname = "Shortname";
+        this.description = "Description";
+        this.modal = "showAddPersonalityModal";
+        this.button = "Add Personality";
+        this.tableRows = "No data loaded";
+        let currentCompany= Company.getInstance();
+        setTimeout(()=>{
+                this._personalityConfigs = currentCompany.companyState.personalities;
+                this.invalidate();},
+            0);
+        currentCompany.onChange((companyState) => {
+            this._personalityConfigs = companyState.personalities;
+            this.invalidate();
+        });
+        document.addEventListener("click", (event) => {
+            let showBox = document.querySelectorAll("div.action-box");
+            showBox.forEach((actionWindow) => {
+                if(actionWindow.style.display === "block")
+                    actionWindow.style.display = "none";
+            });
+        }, true);
+    }
+
+    beforeRender() {
+        this.tableRows="";
+        if(this._personalityConfigs) {
+            this._personalityConfigs.forEach((item) => {
+                this.tableRows += `<personality-item-renderer data-shortname=${item.shortname} data-description="${item.description}" data-primary-key=${item.primaryKey}"></personality-item-renderer>`;
+            });
+        } else {
+            this.tableRows=`<div> No Data Currently </div>`;
+        }
+    }
+    /* adding event Listeners after the web component has loaded, etc */
+    afterRender(){
+
+    }
+}
+
+const onClickOutside = (e) => {
+    if (!e.target.className.includes("action-box")) {
+        e.target.style.display = "none";
+    }
+};
+
+export function showActionBox(primaryKey) {
+    let showBox= document.getElementById(primaryKey);
+    if(showBox.style.display==="none" || showBox.style.display==="") {
+        showBox.style.display = "block";
+    }
+}
