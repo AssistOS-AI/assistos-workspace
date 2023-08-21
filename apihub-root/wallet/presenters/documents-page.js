1cb53d30b5e420c03ff50577c2f075dfccc056e4
alinaduca <alina_duca@yahoo.com>
2023-08-21 08:57:19 +0300

    Adding presenter for documents page and infos in data.json

---
+++ apihub-root/wallet/presenters/documents-page.js
@@ -0,0 +1,43 @@
+import { Company } from "../core/company.js";
+
+export class documentsPage {
+    constructor() {
+        // this.title = "Documents";
+        this.name = "Name";
+        this.status = "Status";
+        this.modal = "showAddaNewDocumentModal";
+        this.button = "Add new document";
+        this.tableRows = "No data loaded";
+        let currentCompany= Company.getInstance();
+        setTimeout(()=> {
+                this._documentConfigs = currentCompany.companyState.documents;
+                this.invalidate();
+        },0);
+        currentCompany.onChange((companyState) => {
+            this._documentConfigs = companyState.documents;
+            this.invalidate();
+        });
+        document.addEventListener("click", (event) => {
+            let showBox = document.querySelectorAll("div.action-box");
+            showBox.forEach((actionWindow) => {
+                if(actionWindow.style.display === "block")
+                    actionWindow.style.display = "none";
+            });
+        }, true);
+    }
+
+    beforeRender() {
+        this.tableRows="";
+        if(this._documentConfigs) {
+            this._documentConfigs.forEach((item) => {
+                this.tableRows += `<document-item-renderer data-name='${item.name}' data-status="${item.status}" data-primary-key=${item.primaryKey}"></document-item-renderer>`;
+            });
+        } else {
+            this.tableRows=`<div> No Data Currently </div>`;
+        }
+    }
+    /* adding event Listeners after the web component has loaded, etc */
+    afterRender(){
+
+    }
+}
