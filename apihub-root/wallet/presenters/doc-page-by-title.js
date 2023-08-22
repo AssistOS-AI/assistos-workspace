3275a62437323567396a2ca237bcaed0bf09c3dd
alinaduca <alina_duca@yahoo.com>
2023-08-22 17:13:46 +0300

    Using web component for chapters in doc-page-by-title

--- apihub-root/wallet/presenters/doc-page-by-title.js
+++ apihub-root/wallet/presenters/doc-page-by-title.js
@@ -1,53 +1,69 @@
 import { Company } from "../core/company.js";
-import { showModal } from "../../WebSkel/utils/modal-utils.js";
 
 export class docPageByTitle {
     constructor() {
-        // this.title = "Documents";
+        this.title = "Documents";
         this.name = "Name";
-        this.modal = "showAddNewDocumentModal";
+        this.abstractText = "Abstract text";
+        if(window.documentSelected !== "undefined") {
+            this.primaryKey = window.documentSelected;
+        }
         this.button = "Add new document";
-        this.tableRows = "No data loaded";
         let currentCompany= Company.getInstance();
         setTimeout(async ()=> {
+            if(window.documentSelected !== "undefined") {
+                this.primaryKey = window.documentSelected;
+            }
             this._documentConfigs = await currentCompany.companyState.documents;
             this.invalidate();
         },0);
-        currentCompany.onChange((companyState) => {
-            // this._documentConfigs = companyState.documents;
+        currentCompany.onChange(async (companyState) => {
+            if(window.selectedDocument !== "undefined") {
+                this.primaryKey = window.selectedDocument;
+            }
+            this._documentConfigs = await companyState.documents;
             this.invalidate();
         });
-        document.addEventListener("click", (event) => {
-            let showBox = document.querySelectorAll("div.action-box");
-            showBox.forEach((actionWindow) => {
-                if(actionWindow.style.display === "block")
-                    actionWindow.style.display = "none";
-            });
-        }, true);
     }
 
     beforeRender() {
-        this.tableRows="";
-        if(this._documentConfigs) {
-            this._documentConfigs.forEach((item) => {
-                this.tableRows += `<document-item-renderer data-name='${item.name}' data-primary-key=${item.primaryKey}"></document-item-renderer>`;
-            });
-        } else {
-            this.tableRows=`<div> No Data Currently </div>`;
+        if(window.selectedDocument !== "undefined") {
+            this.primaryKey = window.selectedDocument;
         }
+        this.chapters="";
+        if(this._documentConfigs) {
+            this._doc = this._documentConfigs.find(document => document.primaryKey === this.primaryKey);
+            try {
+                this.title=this._doc.name;
+                this.abstractText = this._doc.abstract;
+                this._doc.chapters.forEach((item) => {
+                    this.chapters += `<new-chapter data-chapter-title="${item.name}" data-chapter-content="${item.content}"></new-chapter>`;
+                });
+            } catch(e) {}
+        } else {
+            this.chapters=`<div> No Data Currently </div>`;
+        }
+
+
     }
 
     /* adding event Listeners after the web component has loaded, etc */
     afterRender() {
-        // let modalSection = document.querySelector("[data-local-action='showAddNewDocumentModal']");
-        // modalSection.addEventListener("click", async (event) => {
-        //     await showModal(document.querySelector("body"), "add-new-document-modal", {});
-        // });
-        // let editButton = document.querySelector("[data-local-action='editAction']");
-        // console.log(editButton);
-        // editButton.addEventListener("click", async (event) => {
-        //     // await showModal(document.querySelector("body"), "add-new-document-modal", {});
-        //     webSkel.changeToDynamicPage("doc-page-by-title");
-        // });
+        if(window.selectedDocument !== "undefined") {
+            this.primaryKey = window.selectedDocument;
+        }
+        const chapters = document.querySelectorAll('.new-chapter');
+
+        chapters.forEach(chapter => {
+            const title = chapter.querySelector('.chapter-title');
+            const arrow = title.querySelector('.arrow');
+            const content = chapter.querySelector('.chapter-content');
+
+            arrow.addEventListener('click', () => {
+                content.classList.toggle('no-chapter-content');
+                arrow.classList.toggle('rotate');
+            });
+        });
+
     }
 }
