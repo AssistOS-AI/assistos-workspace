d5f771fb7df8c0c3c97b5530184a0c7f62155f21
alinaduca <alina_duca@yahoo.com>
2023-08-24 17:04:30 +0300

    Deleted tools from apihub-components

--- apihub-components/tools/index.js
+++
@@ -1,7 +0,0 @@
-// function Tool(server) {
-//     const { getToolPage } = require("./controller");
-//
-//     server.get("/:domain/tool/:toolId", getToolPage);
-// }
-//
-// module.exports = Tool;
