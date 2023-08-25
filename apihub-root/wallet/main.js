6633db8946deeb476f1b003c9131bf8b78d1505c
Koroem <conduratvictor@yahoo.com>
2023-08-25 09:59:14 +0300

    Added intermediary imports.js file

--- apihub-root/wallet/main.js
+++ apihub-root/wallet/main.js
@@ -1,13 +1,16 @@
-import { llmsPage } from "./presenters/llms-page.js";
-import { personalitiesPage } from "./presenters/personalities-page.js";
-import { documentsPage } from "./presenters/documents-page.js";
-import { docPageByTitle } from "./presenters/doc-page-by-title.js";
-import { editTitlePage } from "./presenters/edit-title-page.js";
-import { editAbstractPage } from "./presenters/edit-abstract-page.js";
-import { proofReaderPage } from "./presenters/proof-reader-page.js";
-import { myOrganisationPage } from "./presenters/my-organisation-page.js";
-import { closeModal, showActionBox } from "../WebSkel/utils/modal-utils.js";
-import WebSkel from "../WebSkel/webSkel.js";
+import {
+    llmsPage,
+    personalitiesPage,
+    documentsPage,
+    docPageByTitle,
+    editTitlePage,
+    editAbstractPage,
+    proofReaderPage,
+    myOrganisationPage,
+    WebSkel,
+    closeModal,
+    showActionBox
+} from "./imports.js";
 
 const openDSU = require("opendsu");
 window.webSkel = new WebSkel();
