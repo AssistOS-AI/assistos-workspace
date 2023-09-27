e7c5f50c149124e6f7dd681d3d89cb8becf287b2
alupoaiemircea <mircea@axiologic.net>
2023-09-27 16:33:54 +0300

    refactoring: replaced sync functions in apihub with async

--- apihub-root/wallet/web-components/pages/authentication-page/authentication-page.js
+++ apihub-root/wallet/web-components/pages/authentication-page/authentication-page.js
@@ -1,4 +1,6 @@
-export class authenticationSection {
+import {extractFormInformation} from "../../../imports.js";
+
+export class authenticationPage {
     constructor(element) {
         this.element = element;
         setTimeout(()=> {
@@ -36,12 +38,156 @@ export class authenticationSection {
                         <input class="form-input" name="password" type="password" data-id="user-password" id="user-password" required placeholder="Add password">
                     </div>
                     <div class="form-footer">
-                        <button class="wide-btn" data-action="beginRegistration">Sign Up</button>
+                        <button class="wide-btn" data-local-action="beginRegistration">Sign Up</button>
                     </div>
                 </form>
            </div>`;
+              break;
+          }
+          case "login-page":{
+              this.subpage = `
+               <div>
+                  <div class="form-title">
+                      Log in
+                  </div>
+                  <form>
+                      <div class="form-item">
+                          <label class="form-label" for="user-email">E-mail</label>
+                          <input class="form-input" name="email" type="email" data-id="user-email" id="user-email" required placeholder="Add e-mail">
+                      </div>
+                      <div class="form-item">
+                          <label class="form-label" for="user-password">Password</label>
+                          <input class="form-input" name="password" type="password" data-id="user-password" id="user-password" required placeholder="Add password">
+                      </div>
+                      <div class="forgot-password" data-local-action="navigateToPasswordRecoveryPage">
+                          Forgot password?
+                      </div>
+                      <div class="form-footer">
+                          <button class="wide-btn" data-action="beginLogin">Log in</button>
+                      </div>
+                      <div class="suggest-registration">
+                          <div>
+                              Don't have an account?
+                          </div>
+                          <div class="sign-up" data-action="navigateToRegisterPage">
+                              Sign Up
+                          </div>
+                      </div>
+                  </form>
+              </div>
+          `;
+              break;
+          }
+          case "register-confirmation":{
+              this.subpage = `
+              <div>
+                  <div class="form-title">
+                   Registration
+                  </div>
+                  <form>
+                   <div class="form-item">
+                        <label class="form-label" for="user-token">Enter the secret token or just click the link we just sent you by email.</label>
+                        <input class="form-input" name="token" type="text" data-email="user-token" id="user-token" required placeholder="Add secret token">
+                    </div>
+                    <div class="form-footer">
+                        <button class="wide-btn" data-action="createUser">Log in</button>
+                    </div>
+                    <div class="development-mode" data-local-action="navigateToLandingPage">
+                        Log in development mode
+                    </div>        
+                </form>
+              </div>`;
+              break;
+          }
+          case "password-recovery":{
+              this.subpage =`
+              <div>
+                  <div class="form-title">
+                   Password Recovery
+                  </div>
+                  <div class="form-description">
+                  Before you introduce your email and new password, we will send you a verification link on your email.
+                  </div>
+                  <form>
+                   <div class="form-item">
+                        <label class="form-label" for="user-email">E-mail</label>
+                        <input class="form-input" name="email" type="email" data-id="user-email" id="user-email" required placeholder="Add e-mail">
+                    </div>
+                    <div class="form-item">
+                        <label class="form-label" for="user-password">New Password</label>
+                        <input class="form-input" name="password" type="password" data-id="user-password" id="user-password" required placeholder="Add new password">
+                    </div>
+                    <div class="form-item">
+                        <label class="form-label" for="user-password-confirm">Confirm new Password</label>
+                        <input class="form-input" name="password-confirm" type="password" data-condition="checkPasswordConfirmation" data-id="user-password-confirm" id="user-password-confirm" required placeholder="Confirm new password">
+                    </div>
+                    <div class="form-footer">
+                        <button class="wide-btn" data-local-action="beginPasswordRecovery">Set New Password</button>
+                    </div>
+                </form>
+              </div>`;
+              break;
+          }
+          case "password-recovery-confirmation":{
+              this.subpage =`
+              <div>
+                  <div class="form-title">
+                   Password Recovery
+                  </div>
+                  <form>
+                   <div class="form-item">
+                        <label class="form-label" for="user-token">Enter the secret token or just click the link we just sent you by email.</label>
+                        <input class="form-input" name="token" type="text" data-id="user-token" id="user-token" required placeholder="Add secret token">
+                    </div>
+                    <div class="form-footer">
+                        <button class="wide-btn" data-local-action="createUser">Log in</button>
+                    </div>
+                    <div class="development-mode" data-local-action="finishPasswordRecovery">
+                        Log in development mode
+                    </div>        
+                </form>
+              </div>`;
+              break;
           }
       }
     }
 
+    async beginRegistration(){
+        const formInfo = await extractFormInformation(formElement);
+        console.log(formInfo)
+        if(formInfo.isValid) {
+            const randomNr = crypto.generateRandom(32);
+            const secretToken = crypto.encrypt(randomNr,crypto.deriveEncryptionKey(formInfo.data.password));
+            const didDocument = await $$.promisify(w3cDID.createIdentity)("key", undefined, randomNr);
+            const result = await $$.promisify(remoteEnclaveClientAccounting.callLambda)("addNewUser",
+                formInfo.data.name,
+                formInfo.data.email,
+                formInfo.data.phone,
+                "publicDescription",
+                secretToken,
+                didDocument.getIdentifier(),
+                "isPrivate");
+            console.log("Add new user command", result);
+            if(typeof result !== "string") {
+                addUserToLocalStorage(result);
+            }
+            this.element.setAttribute("data-subpage", "register-confirmation");
+        }
+    }
+    navigateToPasswordRecoveryPage(){
+        this.element.setAttribute("data-subpage", "password-recovery");
+    }
+    beginPasswordRecovery(){
+        this.element.setAttribute("data-subpage", "password-recovery-confirmation");
+    }
+    finishPasswordRecovery(){
+
+    }
+    navigateToLandingPage(){
+        webSkel.setDomElementForPages(pageContent);
+        window.location = "";
+    }
+    createUser(){
+
+    }
 }
