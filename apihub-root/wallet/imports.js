b48e3c7b0b69c87de73f7951757c96f6d4f172b4
Koroem <conduratvictor@yahoo.com>
2023-10-31 11:05:41 +0200

    imports.js refactor

--- apihub-root/wallet/imports.js
+++ apihub-root/wallet/imports.js
@@ -1,82 +1,48 @@
-import { addDocumentModal } from "./web-components/modals/add-document-modal/add-document-modal.js";
-import { addAnnouncementModal } from "./web-components/modals/add-announcement-modal/add-announcement-modal.js";
-import { addPersonalityModal } from "./web-components/modals/add-personality-modal/add-personality-modal.js";
-import { addUserModal } from "./web-components/modals/add-user-modal/add-user-modal.js";
-import { suggestAbstractModal } from "./web-components/modals/suggest-abstract-modal/suggest-abstract-modal.js";
-import { suggestTitlesModal } from "./web-components/modals/suggest-titles-modal/suggest-titles-modal.js"
-import { documentsPage } from "./web-components/pages/documents-page/documents-page.js";
-import { documentViewPage } from "./web-components/pages/document-view-page/document-view-page.js";
-import { editTitlePage } from "./web-components/pages/edit-title-page/edit-title-page.js";
-import { chapterTitlePage } from "./web-components/pages/chapter-title-page/chapter-title-page.js";
-import { manageParagraphsPage } from "./web-components/pages/manage-paragraphs-page/manage-paragraphs-page.js";
-import { paragraphProofreadPage } from "./web-components/pages/paragraph-proofread-page/paragraph-proofread-page.js";
-import { editAbstractPage } from "./web-components/pages/edit-abstract-page/edit-abstract-page.js";
-import { proofReaderPage } from "./web-components/pages/proof-reader-page/proof-reader-page.js";
-import { spacePage } from "./web-components/pages/space-page/space-page.js";
-import { documentSettingsPage } from "./web-components/pages/document-settings-page/document-settings-page.js";
-import { manageChaptersPage } from "./web-components/pages/manage-chapters-page/manage-chapters-page.js";
-import { personalitiesPage } from "./web-components/subpages/personalities-page/personalities-page.js";
-import { announcementsPage } from "./web-components/subpages/announcements-page/announcements-page.js";
-import { closeModal, showActionBox, showModal, removeActionBox } from "../WebSkel/utils/modal-utils.js";
-import { notBasePage, getClosestParentElement } from "../WebSkel/utils/dom-utils.js";
+// Models
 import { Chapter } from "./core/models/chapter.js";
-import { DocumentModel} from "./core/models/documentModel.js";
+import { DocumentModel } from "./core/models/documentModel.js";
 import { Personality } from "./core/models/personality.js";
 import { User } from "./core/models/user.js";
 import { Announcement } from "./core/models/announcement.js";
 import { Paragraph } from "./core/models/paragraph.js";
 import { Space } from "./core/models/space.js";
-import { spaceDropdown }  from "./web-components/components/space-dropdown/space-dropdown.js";
-import { chapterUnit } from "./web-components/components/item-list/chapter-unit/chapter-unit.js";
-import WebSkel from "../WebSkel/webSkel.js";
-import { extractFormInformation } from "../WebSkel/utils/form-utils.js";
-import { addSpaceModal} from "./web-components/modals/add-space-modal/add-space-modal.js";
 import { Settings } from "./core/models/settings.js";
-import {Script} from "./core/models/script.js";
-import {createFlowsFactory} from "../llmFlows/src/llmFlows.js";
-import {sanitize} from "../WebSkel/utils/dom-utils.js";
-import {reverseQuerySelector} from "../WebSkel/utils/dom-utils.js"
+import { Script } from "./core/models/script.js";
+
 export {
-    removeActionBox,
-    reverseQuerySelector,
-    sanitize,
-    createFlowsFactory,
-    Settings,
-    addSpaceModal,
-    addDocumentModal,
-    addAnnouncementModal,
-    addPersonalityModal,
-    addUserModal,
-    suggestAbstractModal,
-    suggestTitlesModal,
-    documentsPage,
-    documentViewPage,
-    editTitlePage,
-    spaceDropdown,
-    editAbstractPage,
-    proofReaderPage,
-    spacePage,
-    documentSettingsPage,
-    chapterTitlePage,
-    manageParagraphsPage,
-    paragraphProofreadPage,
-    chapterUnit,
-    manageChaptersPage,
-    personalitiesPage,
-    announcementsPage,
-    notBasePage,
-    showModal,
-    closeModal,
-    showActionBox,
-    getClosestParentElement,
-    Space,
-    WebSkel,
     Chapter,
     DocumentModel,
     Personality,
     User,
     Announcement,
     Paragraph,
-    Script,
+    Space,
+    Settings,
+    Script
+};
+
+// Others
+import WebSkel from "../WebSkel/webSkel.js";
+import { createFlowsFactory } from "../llmFlows/src/llmFlows.js";
+
+export {
+    WebSkel,
+    createFlowsFactory
+};
+
+// WebSkel Utilities
+import { closeModal, showActionBox, showModal, removeActionBox } from "../WebSkel/utils/modal-utils.js";
+import { notBasePage, getClosestParentElement, sanitize, reverseQuerySelector } from "../WebSkel/utils/dom-utils.js";
+import { extractFormInformation } from "../WebSkel/utils/form-utils.js";
+
+export {
+    closeModal,
+    showActionBox,
+    showModal,
+    removeActionBox,
+    notBasePage,
+    getClosestParentElement,
     extractFormInformation,
+    sanitize,
+    reverseQuerySelector
 };
