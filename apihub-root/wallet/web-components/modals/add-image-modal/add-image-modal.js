913313582200a898fd8c9d9f5aa1bab90a021d68
Koroem <conduratvictor@yahoo.com>
2023-10-31 19:50:10 +0200

    Add-image-modal structure and imageify page

---
+++ apihub-root/wallet/web-components/modals/add-image-modal/add-image-modal.js
@@ -0,0 +1,9 @@
+class addImageModal{
+    constructor(element,invalidate){
+        this.element=element;
+        this.invalidate=invalidate;
+        this.invalidate();
+    }
+    beforeRender(){}
+    afterRender(){}
+}
