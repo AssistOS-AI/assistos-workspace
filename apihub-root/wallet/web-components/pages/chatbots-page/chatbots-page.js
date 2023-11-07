ed56291f7961f9176ca0c5a8fceb9bc7aa450d1b
alupoaiemircea <mircea@axiologic.net>
2023-11-07 14:19:37 +0200

    update #122

--- apihub-root/wallet/web-components/pages/chatbots-page/chatbots-page.js
+++ apihub-root/wallet/web-components/pages/chatbots-page/chatbots-page.js
@@ -10,37 +10,35 @@ export class chatbotsPage {
         this.history = [];
     }
     beforeRender() {
-        let user = true;
         let stringHTML = "";
         for(let reply of this.history){
-            if(user){
+            if(reply.role === "user"){
                 stringHTML += `
                 <div class="chat-box-container user">
                  <div class="chat-box user-box">${reply}</div>
                 </div>`;
-                user = false;
             }else {
                 stringHTML += `
                 <div class="chat-box-container robot">
                  <div class="chat-box robot-box">${reply}</div>
                 </div>`;
-                user = true;
             }
         }
       this.conversationHistory = stringHTML;
-      this.emotions =`
-      <div class="emotion">
-        <div class="emotion-emoticon">&#128525;</div>
-        <div class="emotion-name">Affection</div>
-      </div>
-      <div class="emotion">
-        <div class="emotion-emoticon">&#128534;</div>
-        <div class="emotion-name">Embarrassment</div>
-      </div>
-      `;
+      // this.emotions =`
+      // <div class="emotion">
+      //   <div class="emotion-emoticon">&#128525;</div>
+      //   <div class="emotion-name">Affection</div>
+      // </div>
+      // <div class="emotion">
+      //   <div class="emotion-emoticon">&#128534;</div>
+      //   <div class="emotion-name">Embarrassment</div>
+      // </div>
+      // `;
     }
     afterRender(){
         this.conversation = this.element.querySelector(".conversation");
+        this.emotionsList = this.element.querySelector(".right-sidebar");
     }
     displayMessage(text, role){
         let reply;
@@ -59,14 +57,35 @@ export class chatbotsPage {
         this.conversation.insertAdjacentHTML("beforeend", reply);
     }
 
+    displayEmotions(currentEmotions){
+        this.emotionsList.textContent = "";
+        let emotions = "";
+        for(let emotion of currentEmotions){
+            emotions += `
+            <div class="emotion">
+             <div class="emotion-emoticon">${emotion.emoji}</div>
+             <div class="emotion-name">${emotion.name}</div>
+           </div>`;
+        }
+        this.emotionsList.insertAdjacentHTML("beforeend", emotions);
+    }
+    summarizeConversation(){
+
+    }
     async sendMessage(_target){
         let formInfo = await extractFormInformation(_target);
-
-        this.displayMessage(formInfo.data.input, "user");
+        let input = formInfo.data.input;
+        formInfo.elements.input.element.value = "";
+        this.displayMessage(input, "user");
         let scriptId = webSkel.space.getScriptIdByName("chatbots");
-        let response = await webSkel.getService("LlmsService").callScript(scriptId, formInfo.data.input, this.personality.name, this.personality.description, this.history);
-        this.history.push(formInfo.data.input);
-        this.history.push(response.responseString);
-        this.displayMessage(response.responseString, "robot");
+        if(this.history.length > 6){
+          this.summarizeConversation();
+        }
+        let response = await webSkel.getService("LlmsService").callScript(scriptId, formInfo.data.input, this.personality.name, this.personality.description, this.history.toSpliced(0,1));
+
+        this.history.push({role:"user",content:input});
+        this.history.push({role:"assistant",content:response.responseJson.reply});
+        this.displayMessage(response.responseJson.reply, "assistant");
+        this.displayEmotions(response.responseJson.emotions);
     }
 }
