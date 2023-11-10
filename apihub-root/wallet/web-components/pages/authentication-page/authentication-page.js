8059c768073354b7ba58f3b7bfb01a4bc984eb95
alupoaiemircea <mircea@axiologic.net>
2023-11-10 11:19:22 +0200

    fix #156

--- apihub-root/wallet/web-components/pages/authentication-page/authentication-page.js
+++ apihub-root/wallet/web-components/pages/authentication-page/authentication-page.js
@@ -5,6 +5,8 @@ export class authenticationPage {
         this.element = element;
         this.invalidate = invalidate;
         this.invalidate();
+
+        this.rotations = 0;
     }
 
     beforeRender() {
@@ -173,19 +175,59 @@ export class authenticationPage {
     }
 
     afterRender(){
-        // let slideIndex = 0;
-        // let slides = document.getElementsByClassName("slides");
-        // const  showSlides = ()=>{
-        //     let i;
-        //     for (i = 0; i < slides.length; i++) {
-        //         slides[i].style.display = "none";
-        //     }
-        //     slideIndex++;
-        //     if (slideIndex > slides.length) {slideIndex = 1}
-        //     slides[slideIndex-1].style.display = "flex";
-        // }
-        // showSlides();
-        // let intervalId = setInterval(showSlides,3000);
+        this.carousel = this.element.querySelector(".images");
+        this.dots = this.element.querySelectorAll(".dot");
+        setTimeout(async()=>{
+           await this.startSlideshow(0);
+        });
+    }
+
+    async startSlideshow(milliseconds){
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+        await delay(milliseconds);
+        this.intervalId = setInterval(this.nextImage.bind(this, "", -1),3000);
+    }
+    async setCurrentImage(_target, position){
+        clearInterval(this.intervalId);
+        this.dots.forEach((dot)=>{
+            dot.classList.remove("active");
+        });
+        _target.classList.add("active");
+        if(position === "1"){
+            this.rotations = 0;
+            this.carousel.style.transform = "translateX(0%)"
+        }else if(position === "2"){
+            this.rotations = -33;
+            this.carousel.style.transform = "translateX(-33%)"
+        }else {
+            this.rotations = -66;
+            this.carousel.style.transform = "translateX(-66%)"
+        }
+    }
+
+    nextImage(_target, direction, stop){
+        if(stop){
+            clearInterval(this.intervalId);
+        }
+        this.dots.forEach((dot)=>{
+            dot.classList.remove("active");
+        });
+        this.rotations += parseInt(direction)* 33;
+        if(this.rotations < -67){
+            this.rotations = 0;
+        }
+        if(this.rotations > 1)
+        {
+            this.rotations = -66;
+        }
+        this.carousel.style.transform = `translateX(${this.rotations}%)`;
+        if(this.rotations === 0){
+            this.dots[0].classList.add("active");
+        }else if(this.rotations === -33){
+            this.dots[1].classList.add("active");
+        }else {
+            this.dots[2].classList.add("active");
+        }
     }
     async navigateToRegisterPage(){
         await webSkel.changeToDynamicPage("authentication-page", "authentication-page",{subpage:"register-page"});
