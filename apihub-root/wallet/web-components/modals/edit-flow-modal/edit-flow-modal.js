22af42a9886ca9b51c280633d237aa2ceef64c88
alupoaiemircea <mircea@axiologic.net>
2023-11-23 14:29:45 +0200

    fix #217

--- apihub-root/wallet/web-components/modals/edit-flow-modal/edit-flow-modal.js
+++ apihub-root/wallet/web-components/modals/edit-flow-modal/edit-flow-modal.js
@@ -18,12 +18,44 @@ export class editFlowModal {
     afterRender(){
         let scriptCode = this.element.querySelector("textarea");
         scriptCode.value = this.scriptContent;
+        scriptCode.addEventListener("keydown", this.insertSpacesOnTab);
     }
 
     closeModal(_target) {
         closeModal(_target);
     }
 
+    insertSpacesOnTab(event){
+        if (event.key === 'Tab' && !event.shiftKey) {
+            event.preventDefault();
+            let start = this.selectionStart;
+            let end = this.selectionEnd;
+            const selectedText = this.value.substring(start, end);
+            const indentedText = selectedText.replace(/^/gm, '    ');
+            this.value = this.value.substring(0, start) + indentedText + this.value.substring(end);
+            this.selectionStart = start + indentedText.length;
+            this.selectionEnd = start + indentedText.length;
+            this.setSelectionRange(this.selectionStart, this.selectionEnd);
+
+        }else if(event.key === 'Tab' && event.shiftKey){
+            event.preventDefault();
+            let start = this.selectionStart;
+            let beginningOfLine = start;
+            while (beginningOfLine > 0 && this.value[beginningOfLine - 1] !== '\n') {
+                beginningOfLine--;
+            }
+            this.selectionStart = beginningOfLine;
+            start = this.selectionStart;
+            let end = this.selectionEnd;
+            const selectedText = this.value.substring(start, end);
+            const unindentedText = selectedText.replace(/^    /gm, '');
+            this.value = this.value.substring(0, start) + unindentedText + this.value.substring(end);
+            const newStart = start;
+            const newEnd = newStart + selectedText.length - unindentedText.length;
+            this.setSelectionRange(newStart, newEnd);
+        }
+
+    }
     async saveScript(_target) {
         let form = this.element.querySelector("form")
         let formInfo = await extractFormInformation(form);
