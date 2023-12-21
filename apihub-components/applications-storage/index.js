362e68aebc176b4320a31d285d4894a818a19781
Koroem <conduratvictor@yahoo.com>
2023-12-21 12:57:47 +0200

    applications operations fixes

--- apihub-components/applications-storage/index.js
+++ apihub-components/applications-storage/index.js
@@ -25,10 +25,12 @@ function ApplicationsStorage(server) {
     server.use("/space/*", bodyReaderMiddleware);
     server.get("/space/:spaceId/applications/:applicationId/configs", loadApplicationConfig);
     server.get("/app/:spaceId/applications/:applicationName/*", loadApplicationComponents);
-    server.put("/space/:spaceId/applications/:applicationId", reinstallApplication);
+
     server.post("/space/:spaceId/applications/:applicationId", installApplication);
     server.delete("/space/:spaceId/applications/:applicationId", uninstallApplication);
+    server.put("/space/:spaceId/applications/:applicationId", reinstallApplication);
+
+
     server.put("/space/:spaceId/applications/:applicationId/:objectType/:objectId", storeObject);
 }
-
 module.exports = ApplicationsStorage;
