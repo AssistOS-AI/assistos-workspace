fcaba6a87bf400efb3baad2d6a306e38f92f1d7c
alupoaiemircea <mircea@axiologic.net>
2023-12-22 09:07:46 +0200

    small fix install app

--- apihub-components/applications-storage/index.js
+++ apihub-components/applications-storage/index.js
@@ -22,7 +22,6 @@ function bodyReaderMiddleware(req, res, next) {
 }
 
 function ApplicationsStorage(server) {
-    server.use("/space/*", bodyReaderMiddleware);
     server.get("/space/:spaceId/applications/:applicationId/configs", loadApplicationConfig);
     server.get("/app/:spaceId/applications/:applicationName/*", loadApplicationComponents);
     server.use("/space/*", bodyReaderMiddleware);
@@ -31,8 +30,6 @@ function ApplicationsStorage(server) {
     server.delete("/space/:spaceId/applications/:applicationId", uninstallApplication);
     server.put("/app/:spaceId/applications/:applicationId/:objectType/:objectId", storeObject);
     server.put("/space/:spaceId/applications/:applicationId", reinstallApplication);
-
-
     server.put("/space/:spaceId/applications/:applicationId/:objectType/:objectId", storeObject);
 }
 
