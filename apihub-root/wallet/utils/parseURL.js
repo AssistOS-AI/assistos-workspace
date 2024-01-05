540a4a00f9665019b5baa866921ccff93d39a1f6
Koroem <conduratvictor@yahoo.com>
2024-01-05 16:14:38 +0200

    Updates and fixes

--- apihub-root/wallet/utils/parseURL.js
+++ apihub-root/wallet/utils/parseURL.js
@@ -1,26 +1,12 @@
 export function parseURL(){
     let url = window.location.hash.split('/');
-    const documents = "documents", space = "#space", chatbots = "#chatbots-page";
     debugger;
     switch(url[2]) {
-        case documents: {
-            let documentId = url[3];
-            let chapterId = url[5];
-            let paragraphId = url[7];
-            if(chapterId){
-                return [documentId, chapterId, paragraphId];
-            }else {
-                return documentId;
-            }
-        }
-        case space:{
-            if(url[4] === "edit-personality-page"){
-                return url[5];
-            }
-            break;
-        }
-        case chatbots:{
+        case "personality": {
             return url[3];
+        }
+        case "applications":{
+            return url[3]
         }
         default:{
             console.error("no parameters for this url");
