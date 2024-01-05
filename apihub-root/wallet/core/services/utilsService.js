7d16e727b22e4334c8ddaf3ac5a85a19ebe7e71d
Koroem <conduratvictor@yahoo.com>
2024-01-05 16:15:57 +0200

    Updates and fixes

--- apihub-root/wallet/core/services/utilsService.js
+++ apihub-root/wallet/core/services/utilsService.js
@@ -20,31 +20,18 @@ export class UtilsService {
     }
      parseURL(){
         let url = window.location.hash.split('/');
-        const documents = "documents", space = "#space", chatbots = "#chatbots-page";
-         switch(url[2]) {
-             case documents: {
-                 let documentId = url[3];
-                 let chapterId = url[5];
-                 let paragraphId = url[7];
-                 if(chapterId){
-                     return [documentId, chapterId, paragraphId];
-                 }else {
-                     return documentId;
-                 }
-             }
-             case space:{
-                 if(url[4] === "edit-personality-page"){
-                     return url[5];
-                 }
-                 break;
-             }
-             case chatbots:{
-                 return url[3];
-             }
-             default:{
-                 console.error("no parameters for this url");
-             }
-         }
+        debugger;
+        switch(url[2]) {
+            case "personality": {
+                return url[3];
+            }
+            case "applications":{
+                return url[3]
+            }
+            default:{
+                console.error("no parameters for this url");
+            }
+        }
     }
     SaveElementTimer(fn, t) {
         return new function (){
