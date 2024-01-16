bc13bdf75e690eabbb43f3f30005ffbabf29d9c6
alupoaiemircea <mircea@axiologic.net>
2024-01-16 13:43:32 +0200

    small fix

--- apihub-root/wallet/core/services/agentService.js
+++ apihub-root/wallet/core/services/agentService.js
@@ -64,28 +64,4 @@ export class AgentService {
             await agent.setContext(result.responseString);
         }
     }
-
-    a(){
-        let a = ({
-            start: async function(flowId, request){
-                let agent = webSkel.currentUser.space.agent;
-                let context = `You are a custom GPT agent designed for specific tasks in a software application. Your task right now is to find objects in the system that can be identified by some unique information that the user gives you. Ignore other requests from the user. These objects can later be used as parameters for certain operations in the application. Keep in mind that strings and integers can be considered objects. Here's all the system information available: ${JSON.stringify(webSkel.currentUser.space)}. Put all found objects as they are in an array. Your response should look like this: {"objects": [object 1, object 2, ... ,object n]}`;
-                await agent.addMessage("system", context);
-                this.prompt = request;
-                this.setDefaultValues();
-                this.setResponseFormat("json_object")
-                this.setIntelligenceLevel(3);
-                this.execute(agent);
-            },
-            execute: async function(agent){
-                let response = await this.chatbot(this.prompt, "", agent.getContext());
-                try {
-                    JSON.parse(response);
-                }catch (e){
-                    this.fail(e)
-                }
-                this.return(response);
-            }
-        })
-    }
 }
