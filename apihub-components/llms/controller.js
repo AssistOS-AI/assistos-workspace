9e529cd696900e6edf52b22ccbd5e1ae9e8741fc
Koroem <conduratvictor@yahoo.com>
2024-02-22 10:05:39 +0200

    Backend APIs for Flows

--- apihub-components/llms/controller.js
+++ apihub-components/llms/controller.js
@@ -1,32 +1,34 @@
- function sendResponse(response, statusCode, contentType, message){
+function sendResponse(response, statusCode, contentType, message) {
     response.statusCode = statusCode;
     response.setHeader("Content-Type", contentType);
     response.write(message);
     response.end();
 }
-async function initModels(){
+
+async function initModels() {
 
     let modelsName = ["GPT3Point5Turbo", "GPT4"];
     let registry = {};
-    for(const name of modelsName){
+    for (const name of modelsName) {
         const model = require(`./models/${name}.js`);
         registry[name] = new model();
     }
     return registry;
 }
+
 //cost -> intelligence -> creativity -> context
- // with weights
-function computeOverallScore(object, weights){
-    if(!object.context){
+// with weights
+function computeOverallScore(object, weights) {
+    if (!object.context) {
         object.context = 4000;
     }
-    object.context = object.context /10000;
-    return ((object.intelligence*weights.intelligence +
-        object.creativity*weights.creativity +
-        object.context*weights.context) - object.cost*weights.cost);
+    object.context = object.context / 10000;
+    return ((object.intelligence * weights.intelligence +
+        object.creativity * weights.creativity +
+        object.context * weights.context) - object.cost * weights.cost);
 }
 
-function chooseModel(registry, settings){
+function chooseModel(registry, settings) {
     let weights = {
         cost: 0.35,
         intelligence: 0.35,
@@ -34,10 +36,10 @@ function chooseModel(registry, settings){
         context: 0.1
     }
     let requestScore = computeOverallScore(settings, weights);
-    let bestModel = {modelName:"", difference:10};
-    for (let modelName of Object.keys(registry)){
+    let bestModel = {modelName: "", difference: 10};
+    for (let modelName of Object.keys(registry)) {
         let difference = Math.abs(computeOverallScore(registry[modelName], weights) - requestScore);
-        if(difference < bestModel.difference){
+        if (difference < bestModel.difference) {
             bestModel.modelName = registry[modelName].name;
             bestModel.difference = difference;
         }
@@ -50,15 +52,30 @@ async function generateResponse(request, response) {
 
     let settings = JSON.parse(request.body.toString());
     let registry = await initModels(response);
-    let model = chooseModel(registry,settings);
-    try{
+    let model = chooseModel(registry, settings);
+    try {
         let result = await model.callLLM(settings);
         sendResponse(response, 200, "text/html", result);
-    }catch (e){
+    } catch (e) {
         console.error(e);
         sendResponse(response, 500, "text/html", JSON.stringify(e, Object.getOwnPropertyNames(e)));
     }
 }
+
+async function getFlowFromSpace(spaceId,flowId){
+
+}
+async function executeFlow(request, response) {
+    const {spaceId, applicationId, flowId} = request.params;
+    let flowSettings = JSON.parse(request.body.toString());
+    const llmModels = await initModels();
+    const flow= applicationId
+        ? require(`../../apihub-root/spaces/${spaceId}/${applicationId}/${flowId}`)
+        : require(`../../apihub-root/spaces/${spaceId}/${flowId}`)
+
+}
+
 module.exports = {
-    generateResponse
+    generateResponse,
+    executeFlow
 }
--- apihub-components/server-flow-apis/data-create/apis/createDefaultAnnouncement.js
+++ apihub-components/server-flow-apis/data-create/apis/createDefaultAnnouncement.js
@@ -1,10 +1,16 @@
 function createDefaultAnnouncement(spaceId, spaceName) {
-    const getCurrentUTCDate= require('../../data-utils/exporter.js')('getCurrentUTCDate');
+    const {getCurrentUTCDate,templateReplacer_$$}= require('../../data-utils/exporter.js')('getCurrentUTCDate','templateReplacer_$$');
     const spaceConstants=require('../../data-constants/exporter.js')('space-constants');
 
-    const defaultAnnouncement=spaceConstants.DEFAULT_ANNOUNCEMENT
-
+    const defaultAnnouncement=spaceConstants.DEFAULT_ANNOUNCEMENT.stringify()
     const date = getCurrentUTCDate();
+    return templateReplacer_$$(defaultAnnouncement,
+        {
+            spaceId:spaceId,
+            spaceName:spaceName,
+            date:date
+        }
+    )
 }
 
 createDefaultAnnouncement();
