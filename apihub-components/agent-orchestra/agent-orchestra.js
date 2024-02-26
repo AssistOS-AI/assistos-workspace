a8ab9d815a913840f861971e6241f426a63ccaa2
Koroem <conduratvictor@yahoo.com>
2024-02-26 14:52:36 +0200

    Make agents observers of the orchestra

--- apihub-components/agent-orchestra/agent-orchestra.js
+++ apihub-components/agent-orchestra/agent-orchestra.js
@@ -2,21 +2,35 @@ class Orchestra {
     constructor(agentRef) {
         // The orchestra conductor: a reference to the main agent that will coordinate the actions.
         this.agent = agentRef;
-        this.executableFlows=OrchestraService.getAllFlows();// or automatically inject them to reduce context.
-        this.activeAgents = [];
-        this.activeFlows = [];
+        this.executableFlows=OrchestraService.getAllFlows();// or inject them to reduce context.
+        this.observeredAgents=[]
+
+    }
+    addObserver(observer) {
+        this.observeredAgents.push(observer);
+    }
+
+    removeObserver(observer) {
+        const index = this.observers.indexOf(observer);
+        if (index > -1) {
+            this.observeredAgents.splice(index, 1);
+        }
+    }
+
+    notifyObservers(event, data) {
+        this.observeredAgents.forEach(observer => observer.update(event, data));
     }
 
     executeFlow(flowName, ...params) {
-
+        /* ....... */
+        this.notifyObservers('flowCompleted', { flowName, params });
     }
     validateFlow(flowName,...params){
-            const result=this.executableFlows[flowName].validate(...params);
+            return this.executableFlows[flowName].validate(...params);
     }
     createAgent(agentConfig) {
-        const newAgent = {};
-        this.activeAgents.push(newAgent);
-        return newAgent;
+        const newAgent = new Agent(configs)
+        this.addObserver(newAgent)
     }
 
 }
