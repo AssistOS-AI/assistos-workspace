2a84dcd7aa36f9e102227984e3a3121d548ecfd0
alupoaiemircea <mircea@axiologic.net>
2024-03-12 17:21:29 +0200

    in progress #20 drag UI

--- apihub-root/wallet/web-components/pages/space-configs-page/space-configs-page.js
+++ apihub-root/wallet/web-components/pages/space-configs-page/space-configs-page.js
@@ -14,6 +14,32 @@ export class SpaceConfigsPage {
         this.expandButton = this.element.querySelector(".expand-button");
         this.expandButton.style.display = "none";
         this.currentPage = this.element.querySelector(".current-page");
+        this.agentPage = this.element.querySelector("agent-page");
+        let resizeBar = this.element.querySelector('.drag-separator');
+        this.isResizing = false;
+
+        if(this.boundMouseDownFn){
+            resizeBar.removeEventListener("mousedown", this.boundMouseDownFn);
+        }
+        this.boundMouseDownFn = this.MouseDownFn.bind(this);
+        resizeBar.addEventListener("mousedown", this.boundMouseDownFn);
+    }
+    resizePanels(event) {
+        if (this.isResizing) {
+            let panel1Width = event.clientX;
+            this.currentPage.style.width = panel1Width + 'px';
+            this.agentPage.style.width = 'calc(100% - ' + panel1Width + 'px)';
+        }
+    }
+
+    stopResize() {
+        this.isResizing = false;
+        this.removeEventListener('mousemove', this.resizePanels);
+    }
+    MouseDownFn(){
+        this.isResizing = true;
+        this.element.addEventListener('mousemove', this.resizePanels);
+        this.element.addEventListener('mouseup', this.stopResize);
     }
 
     async navigateToPage(_target, page){
