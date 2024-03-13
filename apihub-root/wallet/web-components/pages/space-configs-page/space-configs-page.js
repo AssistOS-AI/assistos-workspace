024a7c6e7fa0663732d71082021375f84e35cbbc
alupoaiemircea <mircea@axiologic.net>
2024-03-13 12:28:21 +0200

    in progress #20 drag

--- apihub-root/wallet/web-components/pages/space-configs-page/space-configs-page.js
+++ apihub-root/wallet/web-components/pages/space-configs-page/space-configs-page.js
@@ -23,23 +23,50 @@ export class SpaceConfigsPage {
         }
         this.boundMouseDownFn = this.MouseDownFn.bind(this);
         resizeBar.addEventListener("mousedown", this.boundMouseDownFn);
+
+        if(this.firstNewWidth){
+            this.agentPage.style.width = this.firstNewWidth + 'px';
+            this.currentPage.style.width = this.secondNewWidth + 'px';
+        }
     }
-    resizePanels(event) {
+    resizePanels(startX, firstPanelWidth, secondPanelWidth, event) {
         if (this.isResizing) {
-            let panel1Width = event.clientX;
-            this.currentPage.style.width = panel1Width + 'px';
-            this.agentPage.style.width = 'calc(100% - ' + panel1Width + 'px)';
+            let mouseX = event.clientX;
+            this.firstNewWidth = firstPanelWidth + (mouseX - startX);
+            this.secondNewWidth = secondPanelWidth - (mouseX - startX);
+            let minimumSize = 350;
+            if(this.firstNewWidth > minimumSize && this.secondNewWidth > minimumSize){
+                this.agentPage.style.width = this.firstNewWidth + 'px';
+                this.currentPage.style.width = this.secondNewWidth + 'px';
+            }
+            if(this.secondNewWidth < minimumSize){
+                this.secondNewWidth = minimumSize;
+            }
         }
     }
 
     stopResize() {
         this.isResizing = false;
-        this.removeEventListener('mousemove', this.resizePanels);
+        this.element.removeEventListener('mousemove', this.resizePanels);
+        document.body.style.userSelect = "initial";
     }
-    MouseDownFn(){
+    MouseDownFn(event){
+        document.body.style.userSelect = "none";
         this.isResizing = true;
-        this.element.addEventListener('mousemove', this.resizePanels);
-        this.element.addEventListener('mouseup', this.stopResize);
+        let startX = event.clientX;
+        let firstPanelWidth = parseFloat(getComputedStyle(this.agentPage, null).width);
+        let secondPanelWidth = parseFloat(getComputedStyle(this.currentPage, null).width);
+        if(this.boundMouseMoveFn){
+            this.element.removeEventListener("mousemove", this.boundMouseMoveFn);
+        }
+        this.boundMouseMoveFn = this.resizePanels.bind(this, startX, firstPanelWidth, secondPanelWidth);
+        this.element.addEventListener("mousemove", this.boundMouseMoveFn);
+
+        if(this.boundMouseUp){
+            this.element.removeEventListener("mouseup", this.boundMouseUp);
+        }
+        this.boundMouseUp = this.stopResize.bind(this);
+        this.element.addEventListener("mouseup", this.boundMouseUp);
     }
 
     async navigateToPage(_target, page){
@@ -49,13 +76,13 @@ export class SpaceConfigsPage {
     hideSidebar(){
         this.sidebar.style.transform = "translateX(100%)";
         this.expandButton.style.display = "block";
-        this.currentPage.style.width = "calc((100% + 270px) / 2)";
+        this.currentPage.style.width = this.secondNewWidth ? `calc(${this.secondNewWidth}px + 270px)` : "calc((100% + 270px) / 2)";
         this.dispatchSidebarEvent("hideSidebar");
     }
     showSidebar(){
         this.sidebar.style.transform = "translateX(0%)";
         this.expandButton.style.display = "none";
-        this.currentPage.style.width = "calc((100% - 270px) / 2)";
+        this.currentPage.style.width = this.secondNewWidth ? `${this.secondNewWidth}px` : "calc((100% - 270px) / 2)";
         this.dispatchSidebarEvent("showSidebar");
     }
 
