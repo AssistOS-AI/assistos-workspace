2e8acf92ca20bc8cb355340510c4d891a1e7dff0
alupoaiemircea <mircea@axiologic.net>
2024-03-13 15:08:42 +0200

    fix #20

--- apihub-root/wallet/web-components/pages/space-configs-page/space-configs-page.js
+++ apihub-root/wallet/web-components/pages/space-configs-page/space-configs-page.js
@@ -3,6 +3,7 @@ export class SpaceConfigsPage {
         this.element = element;
         this.invalidate = invalidate;
         this.configPage = location.hash.split("/")[2] || "announcements-page";
+        this.isSidebarVisible = true;
         this.invalidate();
     }
 
@@ -24,23 +25,26 @@ export class SpaceConfigsPage {
         this.boundMouseDownFn = this.MouseDownFn.bind(this);
         resizeBar.addEventListener("mousedown", this.boundMouseDownFn);
 
-        if(this.firstNewWidth){
-            this.agentPage.style.width = this.firstNewWidth + 'px';
-            this.currentPage.style.width = this.secondNewWidth + 'px';
+        if(this.agentPageWidth){
+            this.agentPage.style.width = this.agentPageWidth + 'px';
+            this.currentPage.style.width = this.currentPageWidth + 'px';
         }
     }
     resizePanels(startX, firstPanelWidth, secondPanelWidth, event) {
         if (this.isResizing) {
             let mouseX = event.clientX;
-            this.firstNewWidth = firstPanelWidth + (mouseX - startX);
-            this.secondNewWidth = secondPanelWidth - (mouseX - startX);
+            let firstNewWidth = firstPanelWidth + (mouseX - startX);
+            let secondNewWidth = secondPanelWidth - (mouseX - startX);
             let minimumSize = 350;
-            if(this.firstNewWidth > minimumSize && this.secondNewWidth > minimumSize){
-                this.agentPage.style.width = this.firstNewWidth + 'px';
-                this.currentPage.style.width = this.secondNewWidth + 'px';
-            }
-            if(this.secondNewWidth < minimumSize){
-                this.secondNewWidth = minimumSize;
+            if(firstNewWidth >= minimumSize && secondNewWidth >= minimumSize){
+                this.agentPage.style.width = firstNewWidth + 'px';
+                this.currentPage.style.width = secondNewWidth + 'px';
+                this.agentPageWidth = firstNewWidth;
+                if(this.isSidebarVisible){
+                    this.currentPageWidth = secondNewWidth;
+                } else {
+                    this.currentPageWidth = secondNewWidth - 270;
+                }
             }
         }
     }
@@ -74,15 +78,17 @@ export class SpaceConfigsPage {
     }
 
     hideSidebar(){
+        this.isSidebarVisible = false;
         this.sidebar.style.transform = "translateX(100%)";
         this.expandButton.style.display = "block";
-        this.currentPage.style.width = this.secondNewWidth ? `calc(${this.secondNewWidth}px + 270px)` : "calc((100% + 270px) / 2)";
+        this.currentPage.style.width = this.currentPageWidth ? `calc(${this.currentPageWidth}px + 270px)` : "calc(50% + 135px)";
         this.dispatchSidebarEvent("hideSidebar");
     }
     showSidebar(){
+        this.isSidebarVisible = true;
         this.sidebar.style.transform = "translateX(0%)";
         this.expandButton.style.display = "none";
-        this.currentPage.style.width = this.secondNewWidth ? `${this.secondNewWidth}px` : "calc((100% - 270px) / 2)";
+        this.currentPage.style.width = this.currentPageWidth ? `${this.currentPageWidth}px` : "calc(50% - 135px)";
         this.dispatchSidebarEvent("showSidebar");
     }
 
