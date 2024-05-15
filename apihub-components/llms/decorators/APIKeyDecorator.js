7fd36970a459c3e207bdc5aeb10eeae2492c3604
Koroem <conduratvictor@yahoo.com>
2024-05-15 14:21:28 +0300

    Missing await

--- apihub-components/llms/decorators/APIKeyDecorator.js
+++ apihub-components/llms/decorators/APIKeyDecorator.js
@@ -1,11 +1,11 @@
 const secrets = require('../../apihub-component-utils/secrets.js');
 
-module.exports = async function(spaceId, modelName, callbackFunc, ...callbackArgs) {
-    const APIKey = secrets.getModelAPIKey(spaceId, modelName);
+module.exports = async function(spaceId, modelName, callbackFunc, ...callbackFuncArgs) {
+    const APIKey = await secrets.getModelAPIKey(spaceId, modelName);
     if (!APIKey) {
         const error = new Error("API key not found");
         error.statusCode = 400;
         throw error;
     }
-    return await callbackFunc(APIKey, ...callbackArgs);
+    return await callbackFunc(APIKey, ...callbackFuncArgs);
 }
