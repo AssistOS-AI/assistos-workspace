f5acdfc0c30668983aadf275ea798a782921b32b
Koroem <conduratvictor@yahoo.com>
2024-05-24 14:48:46 +0300

    Refactor and fixes for LLMs

--- apihub-components/llms/__archived/interfaces/IMultiLLM.js
+++
@@ -1,8 +0,0 @@
-class IMultiLLM {
-    constructor(){
-        if (new.target === IMultiLLM) {
-            throw new Error("ILLM cannot be instantiated directly.");
-        }
-    }
-
-}
--- apihub-components/llms/__archived/utils/streamEmitter.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter = require('events');
-
-class LLMStreamingEmitter extends EventEmitter {}
-
-const streamEmitter = new LLMStreamingEmitter();
-
-module.exports = streamEmitter;
