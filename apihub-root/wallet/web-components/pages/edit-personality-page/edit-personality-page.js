f00c299937857200191636c39c29775494f757cf
alupoaiemircea <mircea@axiologic.net>
2024-06-12 11:34:24 +0300

    update select voice on personality

--- apihub-root/wallet/web-components/pages/edit-personality-page/edit-personality-page.js
+++ apihub-root/wallet/web-components/pages/edit-personality-page/edit-personality-page.js
@@ -2,42 +2,51 @@ const constants = require("assistos").constants;
 const spaceAPIs = require("assistos").loadModule("space", {});
 const {notificationService} = require("assistos").loadModule("util", {});
 const llmModule = require("assistos").loadModule("llm", {});
-export class EditPersonalityPage{
-    constructor(element,invalidate) {
+
+export class EditPersonalityPage {
+    constructor(element, invalidate) {
         this.element = element;
-        this.invalidate=invalidate;
+        this.invalidate = invalidate;
         this.knowledgeArray = [];
-        this.refreshPersonality = async ()=>{
+        this.refreshPersonality = async () => {
             this.personality = await assistOS.space.getPersonality(window.location.hash.split("/")[3]);
         }
-        this.invalidate(async ()=>{
+        this.invalidate(async () => {
             await this.refreshPersonality();
             spaceAPIs.startCheckingUpdates(assistOS.space.id);
             spaceAPIs.subscribeToObject(assistOS.space.id, this.personality.id);
-            notificationService.on(this.personality.id, ()=>{
+            notificationService.on(this.personality.id, () => {
                 this.invalidate(this.refreshPersonality);
             });
-            notificationService.on(this.personality.id + "/delete", async ()=>{
+            notificationService.on(this.personality.id + "/delete", async () => {
                 await this.openPersonalitiesPage();
                 await spaceAPIs.unsubscribeFromObject(assistOS.space.id, this.personality.id);
                 spaceAPIs.stopCheckingUpdates(assistOS.space.id);
                 alert("The personality has been deleted");
             })
+            let configs = await llmModule.listVoicesAndEmotions(assistOS.space.id);
+            this.voices = configs.voices;
         });
     }
-    beforeRender(){
-        if(this.personality.name === constants.PERSONALITIES.DEFAULT_PERSONALITY_NAME){
+
+    beforeRender() {
+        let voicesHTML = "";
+        for (let voice of this.voices) {
+            voicesHTML += `<option value="${voice.id}">${voice.name}, accent: ${voice.accent}, age: ${voice.age}, gender: ${voice.gender}, loudness: ${voice.loudness}, tempo: ${voice.tempo}</option>`;
+        }
+        this.voicesOptions = voicesHTML;
+        if (this.personality.name === constants.PERSONALITIES.DEFAULT_PERSONALITY_NAME) {
             this.disabled = "disabled";
         }
-        if(this.personality.image){
+        if (this.personality.image) {
             this.photo = this.personality.image;
         } else {
             this.photo = "./wallet/assets/images/default-personality.png";
         }
         this.personalityName = this.personality.name;
         let string = "";
-        for(let fact of this.knowledgeArray){
-            string+= `<div class="fact">${fact}</div>`;
+        for (let fact of this.knowledgeArray) {
+            string += `<div class="fact">${fact}</div>`;
         }
         this.filteredKnowledge = string;
 
@@ -47,18 +56,20 @@ export class EditPersonalityPage{
         // }
         // this.llmOptions = llmOptions;
     }
-    preventRefreshOnEnter(event){
-        if(event.key === "Enter"){
+
+    preventRefreshOnEnter(event) {
+        if (event.key === "Enter") {
             event.preventDefault();
             this.element.querySelector(".magnifier-container").click();
         }
     }
+
     async afterUnload() {
         await spaceAPIs.unsubscribeFromObject(assistOS.space.id, this.personality.id);
         spaceAPIs.stopCheckingUpdates(assistOS.space.id);
     }
 
-    afterRender(){
+    afterRender() {
         let description = this.element.querySelector("textarea");
         description.innerHTML = this.personality.description;
         this.userInput = this.element.querySelector("#search");
@@ -67,11 +78,39 @@ export class EditPersonalityPage{
         this.userInput.addEventListener("keypress", this.boundFn);
 
         let photoInput = this.element.querySelector("#photo");
-        if(this.boundShowPhoto){
+        if (this.boundShowPhoto) {
             photoInput.removeEventListener("input", this.boundShowPhoto);
         }
-        this.boundShowPhoto =  this.showPhoto.bind(this, photoInput)
+        this.boundShowPhoto = this.showPhoto.bind(this, photoInput)
         photoInput.addEventListener("input", this.boundShowPhoto);
+        let voiceSelect = this.element.querySelector("#voiceId");
+        if(this.personality.voiceId){
+            let audioSource = this.element.querySelector('.audio-source');
+            let audioSection = this.element.querySelector(".audio-section");
+            let voice = this.voices.find(voice => voice.id === this.personality.voiceId);
+            audioSection.classList.remove("hidden");
+            voiceSelect.value = this.personality.voiceId;
+            audioSource.src = voice.sample;
+            audioSource.load();
+        }
+
+        if (!this.boundSelectVoiceHndler) {
+            this.boundSelectVoiceHndler = this.selectVoiceHandler.bind(this, voiceSelect);
+            voiceSelect.addEventListener("change", this.boundSelectVoiceHndler);
+        }
+    }
+
+    selectVoiceHandler(voiceSelect, event) {
+        let audioSection = this.element.querySelector(".audio-section");
+        if (voiceSelect.value) {
+            let audioSource = this.element.querySelector('.audio-source');
+            let voice = this.voices.find(voice => voice.id === voiceSelect.value);
+            audioSection.classList.remove("hidden");
+            audioSource.src = voice.sample;
+            audioSource.load();
+        } else {
+            audioSection.classList.add("hidden");
+        }
     }
 
     async showPhoto(photoInput, event) {
@@ -81,36 +120,42 @@ export class EditPersonalityPage{
         this.photo = encodedPhoto;
     }
 
-    async search(_target){
+    async search(_target) {
         let form = this.element.querySelector(".search");
         let formInfo = await assistOS.UI.extractFormInformation(form);
         this.knowledgeArray = JSON.parse(await assistOS.space.getAgent().loadFilteredKnowledge(assistOS.space.id, formInfo.data.search));
-        if(this.knowledgeArray.length === 0){
+        if (this.knowledgeArray.length === 0) {
             this.knowledgeArray = ["Nothing found"];
         }
         this.invalidate();
     }
 
-    triggerInputFileOpen(_target, id){
+    triggerInputFileOpen(_target, id) {
         _target.removeAttribute("data-local-action");
         let input = this.element.querySelector(`#${id}`);
         input.click();
         _target.setAttribute("data-local-action", `triggerInputFileOpen ${id}`);
     }
 
-    async saveChanges(_target){
+    async saveChanges(_target) {
         const verifyPhotoSize = (element) => {
-            if(element.files.length > 0){
+            if (element.files.length > 0) {
                 return element.files[0].size <= 1048576;
             }
             return true;
         };
-        const conditions = {"verifyPhotoSize": {fn:verifyPhotoSize, errorMessage:"Image too large! Image max size: 1MB"} };
+        const conditions = {
+            "verifyPhotoSize": {
+                fn: verifyPhotoSize,
+                errorMessage: "Image too large! Image max size: 1MB"
+            }
+        };
         let formInfo = await assistOS.UI.extractFormInformation(_target, conditions);
-        if(formInfo.isValid) {
+        if (formInfo.isValid) {
             this.personality.name = formInfo.data.name;
             this.personality.description = formInfo.data.description;
             this.personality.image = this.photo;
+            this.personality.voiceId = formInfo.data.voiceId;
             await assistOS.callFlow("UpdatePersonality", {
                 spaceId: assistOS.space.id,
                 personalityData: this.personality,
@@ -119,11 +164,12 @@ export class EditPersonalityPage{
             await this.openPersonalitiesPage();
         }
     }
-    async addKnowledge(_target){
+
+    async addKnowledge(_target) {
         let formInfo = await assistOS.UI.extractFormInformation(_target);
         let promiseArray = [];
-        if(formInfo.isValid){
-            for(let file of formInfo.data.files){
+        if (formInfo.isValid) {
+            for (let file of formInfo.data.files) {
                 promiseArray.push(await assistOS.UI.uploadFileAsText(file));
             }
             let files = await Promise.all(promiseArray);
@@ -131,7 +177,7 @@ export class EditPersonalityPage{
         }
     }
 
-    async deletePersonality(){
+    async deletePersonality() {
         await assistOS.callFlow("DeletePersonality", {
             spaceId: assistOS.space.id,
             personalityId: this.personality.id
@@ -139,7 +185,7 @@ export class EditPersonalityPage{
         await this.openPersonalitiesPage();
     }
 
-    async openPersonalitiesPage(){
-      await assistOS.UI.changeToDynamicPage("space-configs-page", `${assistOS.space.id}/Space/personalities-page`);
+    async openPersonalitiesPage() {
+        await assistOS.UI.changeToDynamicPage("space-configs-page", `${assistOS.space.id}/Space/personalities-page`);
     }
 }
