aa15a5849fa54cc421ab15d0020319919c72167f
Koroem <conduratvictor@yahoo.com>
2024-09-25 16:29:07 +0300

    Fix

--- apihub-components/apihub-component-utils/fileSys.js
+++ apihub-components/apihub-component-utils/fileSys.js
@@ -154,5 +154,6 @@ module.exports = {
     getVideoStream,
     headAudio,
     headVideo,
-    headImage
+    headImage,
+    downloadData
 }
--- apihub-components/tasks/LipSync.js
+++ apihub-components/tasks/LipSync.js
@@ -4,6 +4,10 @@ const constants = require('./constants');
 const STATUS = constants.STATUS;
 const EVENTS = constants.EVENTS;
 const TaskManager = require('./TaskManager');
+const path = require('path');
+const space = require("../spaces-storage/space");
+const fsPromises = require('fs').promises;
+
 class LipSync extends Task {
     constructor(securityContext, spaceId, userId, configs) {
         super(securityContext, spaceId, userId);
@@ -26,8 +30,8 @@ class LipSync extends Task {
 
                 const paragraphCommands = paragraph.commands;
                 let speechCommand = paragraphCommands.speech;
-                if(!paragraphCommands.audio){
-                    if(!speechCommand){
+                if (!paragraphCommands.audio) {
+                    if (!speechCommand) {
                         await this.rollback();
                         this.rejectTask("Paragraph Must have a speech command before adding lip sync");
                     } else {
@@ -38,13 +42,13 @@ class LipSync extends Task {
                             this.setStatus(STATUS.RUNNING);
                             let paragraphCommands = await documentModule.getParagraphCommands(this.spaceId, this.documentId, this.paragraphId);
                             paragraphCommands.audio = {id: paragraphCommands.audio.id};
-                            await this.executeLipSync(spaceModule,llmModule, utilModule, paragraphCommands);
+                            await this.executeLipSync(spaceModule, llmModule, utilModule, paragraphCommands);
                         });
                         this.setStatus(STATUS.PENDING);
                         return this.taskPromise;
                     }
                 }
-                await this.executeLipSync(spaceModule,llmModule, utilModule, paragraphCommands);
+                await this.executeLipSync(spaceModule, llmModule, utilModule, paragraphCommands);
             } catch (e) {
                 await this.rollback();
                 this.rejectTask(e);
@@ -52,26 +56,38 @@ class LipSync extends Task {
         });
         return this.taskPromise;
     }
-    async executeLipSync(spaceModule,llmModule, utilModule, paragraphCommands) {
+
+    async executeLipSync(spaceModule, llmModule, utilModule, paragraphCommands) {
         this.timeout = setTimeout(async () => {
             await this.rollback();
             this.rejectTask(new Error("Task took too long to complete"));
         }, 60000 * 10);
-        if(paragraphCommands.video){
+        if (paragraphCommands.video) {
             await llmModule.lipSync(this.spaceId, this.id, paragraphCommands.video.id, paragraphCommands.audio.id, "sync-1.6.0");
         } else {
             const imageSrc = utilModule.constants.getImageSrc(this.spaceId, paragraphCommands.image.id);
             const audioSrc = utilModule.constants.getAudioSrc(this.spaceId, paragraphCommands.audio.id);
-            const videoId = await ffmpeg.createVideoFromImageAndAudio(imageSrc, audioSrc, this.spaceId,this.securityContext);
+            const videoId = await ffmpeg.createVideoFromImageAndAudio(imageSrc, audioSrc, this.spaceId, this.securityContext);
             await llmModule.lipSync(this.spaceId, this.id, videoId, paragraphCommands.audio.id, "sync-1.6.0");
         }
     }
+
     async completeTaskExecution(videoURL) {
         clearTimeout(this.timeout);
         delete this.timeout;
         const spaceModule = require('assistos').loadModule('space', this.securityContext);
         const documentModule = require('assistos').loadModule('document', this.securityContext);
-        const videoId = await spaceModule.addVideo(this.spaceId, videoURL);
+
+        const fileSys = require('../apihub-component-utils/fileSys.js')
+        const space = require('../spaces-storage/space.js')
+        const crypto = require('../apihub-component-utils/crypto.js')
+
+        const tempFileId = crypto.generateId();
+        const tempFilePath = path.join(space.APIs.getSpacePath(this.spaceId), "temp", `${tempFileId}.mp4`);
+        await fileSys.downloadData(videoURL, tempFilePath);
+
+        const videoBuffer = await fsPromises.readFile(tempFilePath);
+        const videoId = await spaceModule.addVideo(this.spaceId, videoBuffer);
         const paragraphCommands = await documentModule.getParagraphCommands(this.spaceId, this.documentId, this.paragraphId);
         paragraphCommands.video = {id: videoId};
         delete paragraphCommands.lipsync.taskId;
@@ -109,12 +125,13 @@ class LipSync extends Task {
             }
         }
     }
+
     async getRelevantInfo() {
         let info = {
             documentId: this.documentId,
             paragraphId: this.paragraphId
         }
-        if(this.status === STATUS.FAILED){
+        if (this.status === STATUS.FAILED) {
             info.failMessage = this.failMessage;
         }
         return info;
