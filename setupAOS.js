cbd465f747c3b9638ed807ebc1b12f6713d6d899
alupoaiemircea <mircea@axiologic.net>
2024-10-14 13:02:35 +0300

    update dev-install

--- setupAOS.js
+++ setupAOS.js
@@ -1,13 +1,40 @@
-const configs = require('./config.json');
-const cleanStorageVolume = configs.CLEAN_STORAGE_VOLUME_ON_RESTART;
+const fs = require('fs');
+const path = require('path');
+const directoriesToCreate = [
+    "spaces",
+    "users",
+    "Temp"
+];
+const mainDir = "data-volume";
+const filesToCreate = [
+    "[MAP]Spaces.json",
+    "[MAP]Users.json",
+    "SpacesPendingInvitations.json",
+    "user-credentials.json",
+    "UsersPendingActivation.json"
+];
 (async () => {
-    let setupPromises = [];
-
-    if (cleanStorageVolume) {
-        const volume = require('./data-volume');
+    try {
+        await Promise.all(directoriesToCreate.map(async dir => {
+            let dirPath = path.join(__dirname, mainDir, dir);
+            try {
+                await fs.promises.access(dirPath);
+            } catch (error) {
+                await fs.promises.mkdir(dirPath);
+                console.log(`Created dir: ${dirPath}`);
+            }
+        }));
+        await Promise.all(filesToCreate.map(async file => {
+            let filePath = path.join(__dirname, mainDir, file);
+            try{
+                await fs.promises.access(filePath);
+            }catch (e) {
+                await fs.promises.writeFile(filePath, "{}");
+                console.log(`Created file: ${file}`);
+            }
+        }));
+    } catch (e) {
+        console.error("AOS setup failed: " + e);
     }
-
-    await Promise.all(setupPromises);
-})()
-
+})();
 
