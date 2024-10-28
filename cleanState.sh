1e9f9947ba4976227fd6b4188b792fae8c8c04ac
alupoaiemircea <mircea@axiologic.net>
2024-10-28 13:37:05 +0200

    files cleanup + clean command as script

---
+++ cleanState.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf data-volume/spaces/*
+rm -rf data-volume/users/*
+echo "{}" > data-volume/[MAP]Spaces.json
+echo "{}" > data-volume/[MAP]Users.json
+echo "{}" > data-volume/user-credentials.json
+echo "{}" > data-volume/UsersPendingActivation.json
+echo "{}" > data-volume/SpacesPendingInvitations.json
+rm -rf ./data-volume/versionlessdsu
+rm -rf ./data-volume/secrets/*
+find ./data-volume/lightDB -mindepth 1 -type d ! -name 'FixedUrls.db' -exec rm -rf {} +
+rm -rf ./data-volume/assets/images/*
+rm -rf ./data-volume/assets/videos/*
+rm -rf ./data-volume/assets/audios/*
