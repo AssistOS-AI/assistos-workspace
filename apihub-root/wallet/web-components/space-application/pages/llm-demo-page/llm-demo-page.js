5f5ab321697c254fab1c084b4ed36edb5ee261d4
alupoaiemircea <mircea@axiologic.net>
2024-11-04 10:23:09 +0200

    add llm page

---
+++ apihub-root/wallet/web-components/space-application/pages/llm-demo-page/llm-demo-page.js
@@ -0,0 +1,13 @@
+export class LlmDemoPage{
+    constructor(element, invalidate){
+        this.element = element;
+        this.invalidate = invalidate;
+        this.invalidate();
+    }
+    beforeRender(){
+
+    }
+    afterRender(){
+
+    }
+}
