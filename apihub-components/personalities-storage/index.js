4044dccb7efeacdf61fc6621fb2c001cc24b0d43
VictorCondurat <conduratvictor@yahoo.com>
2024-11-19 15:49:07 +0200

    feat(LLM): ensure new and existing spaces have personalities with default LLMs
    - Added a quickfix to update existing personalities with default LLMs withing existing spaces
    - Added the personalities Component to apihub.json
    
    Closes #297

--- apihub-components/personalities-storage/index.js
+++ apihub-components/personalities-storage/index.js
@@ -1,12 +1,11 @@
 const bodyReader=require("../apihub-component-middlewares/bodyReader");
+const {
+    ensurePersonalitiesDefaultLllms
+} = require("./controller");
 
 function PersonalitiesStorage(server){
-    const { loadKnowledge, loadFilteredKnowledge, addKnowledge, storeKnowledge } = require("./controller");
-    server.get("/personalities/:spaceId/:personalityId", loadKnowledge);
-    server.get("/personalities/:spaceId/:personalityId/search", loadFilteredKnowledge);
     server.use("/personalities/*", bodyReader);
-    server.put("/personalities/:spaceId/:personalityId/add", addKnowledge);
-    server.put("/personalities/:spaceId/:personalityId/store", storeKnowledge);
+    server.post("/personalities/:spaceId/ensure-default-llms",ensurePersonalitiesDefaultLllms);
 }
 
 module.exports = PersonalitiesStorage;
