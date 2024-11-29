c485fef92b54e28047f3212a22989455d866705e
alupoaiemircea <mircea@axiologic.net>
2024-11-29 16:49:35 +0200

    update export document

---
+++ apihub-components/tasks/ExportDocument.js
@@ -0,0 +1,200 @@
+const fs = require('fs');
+const Task = require('./Task');
+const path = require('path');
+const space = require('../spaces-storage/space');
+const Storage = require("../apihub-component-utils/storage");
+const constants = require('./constants');
+const archiver = require("archiver");
+const crypto = require("../apihub-component-utils/crypto");
+const STATUS = constants.STATUS;
+class ExportDocument extends Task {
+    constructor(spaceId, userId, configs) {
+        super(spaceId, userId);
+        this.processes = [];
+        this.documentId = configs.documentId;
+        this.exportType = configs.exportType;
+    }
+    async runTask(){
+        return new Promise(async (resolve, reject) => {
+            const documentModule = await this.loadModule('document');
+            let document = await documentModule.getDocument(this.spaceId, this.documentId);
+
+            let documentData;
+            if (this.exportType === 'full') {
+                documentData = await this.exportDocumentData(document, this.spaceId);
+            } else {
+                documentData = await this.exportDocumentDataPartially(document, this.spaceId);
+            }
+
+            const archive = archiver('zip', {zlib: {level: 9}});
+            this.archive = archive;
+            let spacePath = space.APIs.getSpacePath(this.spaceId);
+            await fs.promises.mkdir(path.join(spacePath, "temp"), {recursive: true});
+            let archiveName = `${this.id}.docai`;
+            let outputPath = path.join(spacePath, "temp", archiveName);
+            let outputStream = fs.createWriteStream(outputPath);
+
+            archive.pipe(outputStream);
+            archive.on('error', err => {
+                reject(err);
+            });
+            outputStream.on('error', err => {
+                reject(err);
+            });
+            archive.on('finish', () => {
+                resolve(archiveName);
+            });
+
+            const contentBuffer = Buffer.from(JSON.stringify(documentData), 'utf-8');
+            const checksum = require('crypto').createHash('sha256').update(contentBuffer).digest('hex');
+            const metadata = {
+                title: documentData.title,
+                created: new Date().toISOString(),
+                modified: new Date().toISOString(),
+                version: "1.0",
+                checksum: checksum,
+                contentFile: "data.json",
+            };
+            archive.append(contentBuffer, {name: 'data.json'});
+            archive.append(Buffer.from(JSON.stringify(metadata), 'utf-8'), {name: 'metadata.json'});
+            for(let personalityId of documentData.personalities){
+                const personalityStream = await space.APIs.archivePersonality(this.spaceId, personalityId);
+                archive.append(personalityStream, {name: `personalities/${personalityId}.persai`});
+            }
+
+            for(let imageData of documentData.images){
+                const imageName = imageData.name;
+                const {fileStream, headers} = await Storage.getFile(Storage.fileTypes.images, imageData.id);
+                archive.append(fileStream, {name: `images/${imageName}.png`});
+            }
+
+            for(let audioData of documentData.audios){
+                const audioName = audioData.name;
+                const {fileStream, headers} = await Storage.getFile(Storage.fileTypes.audios, audioData.id);
+                archive.append(fileStream, {name: `audios/${audioName}.mp3`});
+            }
+            for(let videoData of documentData.videos){
+                const videoName = videoData.name;
+                const {fileStream, headers} = await Storage.getFile(Storage.fileTypes.videos, videoData.id);
+                archive.append(fileStream, {name: `videos/${videoName}.mp4`});
+            }
+            await archive.finalize();
+        });
+    }
+    async exportDocumentDataPartially(document, spaceId) {
+        let personalities = new Set();
+        document.exportType = "partial";
+        document.images = [];
+        document.audios = [];
+        document.videos = [];
+        for (let chapter of document.chapters) {
+            for (let paragraph of chapter.paragraphs) {
+                if (paragraph.commands.speech) {
+                    const personality = paragraph.commands.speech.personality;
+                    personalities.add(personality);
+                }
+            }
+        }
+        document.personalities = await space.APIs.getPersonalitiesIds(spaceId, personalities);
+        return document;
+    }
+
+    async exportDocumentData(document, spaceId) {
+        document.exportType = "full";
+        let audios = [];
+        let images = [];
+        let videos = [];
+        let personalities = new Set();
+        for (let chapter of document.chapters) {
+            const chapterIndex = document.chapters.indexOf(chapter);
+            if (chapter.backgroundSound) {
+                chapter.backgroundSound.fileName = `Chapter_${chapterIndex + 1}_audio`
+                audios.push({
+                    name: chapter.backgroundSound.fileName,
+                    id: chapter.backgroundSound.id
+                })
+            }
+            for (let paragraph of chapter.paragraphs) {
+                const paragraphIndex = chapter.paragraphs.indexOf(paragraph);
+                if(paragraph.commands.audio) {
+                    paragraph.commands.audio.fileName = `Chapter_${chapterIndex + 1}_Paragraph_${paragraphIndex + 1}_audio`
+                    audios.push({
+                        name: paragraph.commands.audio.fileName,
+                        id: paragraph.commands.audio.id
+                    })
+                }
+                if (paragraph.commands.effects) {
+                    for(let effect of paragraph.commands.effects){
+                        effect.fileName = `Chapter_${chapterIndex + 1}_Paragraph_${paragraphIndex + 1}_effect_${effect.name}_${effect.id}`;
+                        audios.push({
+                            name: effect.fileName,
+                            id: effect.id
+                        })
+                    }
+                }
+                if(paragraph.commands.speech) {
+                    const personality = paragraph.commands["speech"].personality;
+                    personalities.add(personality);
+                }
+                if (paragraph.commands.image) {
+                    paragraph.commands.image.fileName = `Chapter_${chapterIndex + 1}_Paragraph_${paragraphIndex + 1}_image`
+                    images.push({
+                        name: paragraph.commands.image.fileName,
+                        id: paragraph.commands.image.id
+                    })
+                }
+                if (paragraph.commands.video) {
+                    paragraph.commands.video.fileName = `Chapter_${chapterIndex + 1}_Paragraph_${paragraphIndex + 1}_video`
+                    videos.push({
+                        name: paragraph.commands.video.fileName,
+                        id: paragraph.commands.video.id
+                    })
+                    if(paragraph.commands.video.thumbnailId){
+                        paragraph.commands.video.thumbnailFileName = `${paragraph.commands.video.fileName}_thumbnail`
+                        images.push({
+                            name: paragraph.commands.video.thumbnailFileName,
+                            id: paragraph.commands.video.thumbnailId
+                        })
+                    }
+                }
+            }
+        }
+        document.images = images;
+        document.audios = audios;
+        document.videos = videos;
+        document.personalities = await space.APIs.getPersonalitiesIds(spaceId, personalities);
+        return document;
+    }
+    async cancelTask(){
+        this.archive.emit('error', "Task was cancelled");
+    }
+
+
+    serialize() {
+        return{
+            id: this.id,
+            status: this.status,
+            spaceId: this.spaceId,
+            userId: this.userId,
+            name: this.constructor.name,
+            configs:{
+                spaceId: this.spaceId,
+                documentId: this.documentId,
+            }
+        }
+    }
+
+
+    async getRelevantInfo() {
+        if (this.status === STATUS.RUNNING) {
+            return `Exporting document ${this.document.title}`;
+        }
+        if (this.status === STATUS.FAILED) {
+            return this.failMessage;
+        }
+        if (this.status === STATUS.COMPLETED) {
+            return `Document ${this.document.title} export completed`;
+        }
+    }
+}
+module.exports = ExportDocument;
--- apihub-root/wallet/web-components/document/modals/export-document-modal/export-document-modal.js
+++ apihub-root/wallet/web-components/document/modals/export-document-modal/export-document-modal.js
@@ -1,10 +1,12 @@
 const documentModule = require('assistos').loadModule('document', {});
+import {NotificationRouter} from '../../../../imports.js';
 export class ExportDocumentModal{
     constructor(element, invalidate) {
         this.element = element;
         this.invalidate = invalidate;
         this.documentId = this.element.getAttribute('data-id');
         this.documentTitle = this.element.getAttribute('data-title');
+        this.boundsOnCompleteExport = this.onCompleteExport.bind(this);
         this.invalidate();
     }
     beforeRender() {
@@ -13,32 +15,45 @@ export class ExportDocumentModal{
     async closeModal() {
         await assistOS.UI.closeModal(this.element);
     }
+    async onCompleteExport(status) {
+        if(status === "failed"){
+            showApplicationError("Export failed", data.error);
+            assistOS.UI.closeModal(this.element);
+        } else if(status === "completed") {
+            let exportButton = this.element.querySelector('.export-button');
+            exportButton.classList.remove('loading-icon');
+            exportButton.innerHTML = 'Download';
+            exportButton.setAttribute('data-local-action', `downloadArchive`);
+        }
+    }
+
+    async downloadArchive(targetElement) {
+        const url = `/documents/export/${assistOS.space.id}/${this.taskId}`;
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        if(this.exportType === 'full') {
+            a.download = `${assistOS.UI.unsanitize(this.documentTitle)}.docai`;
+        }
+        else {
+            a.download = `${assistOS.UI.unsanitize(this.documentTitle)}_partial.docai`;
+        }
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        await assistOS.UI.closeModal(this.element);
+        delete this.taskId;
+    }
+
     async exportDocument(button) {
         button.classList.add('loading-icon');
         button.innerHTML = '';
+
         let checkBox = this.element.querySelector('input[type="checkbox"]');
-        let exportType = checkBox.checked ? 'full' : 'partial';
+        this.exportType = checkBox.checked ? 'full' : 'partial';
         try {
-            let archive = await documentModule.exportDocument(assistOS.space.id, this.documentId, exportType);
-            const url = window.URL.createObjectURL(archive);
-
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = url;
-            if(exportType === 'full') {
-                a.download = `${assistOS.UI.unsanitize(this.documentTitle)}.docai`;
-            }
-            else {
-                a.download = `${assistOS.UI.unsanitize(this.documentTitle)}_partial.docai`;
-            }
-            document.body.appendChild(a);
-            a.click();
-
-            window.URL.revokeObjectURL(url);
-            document.body.removeChild(a);
-            button.classList.remove('loading-icon');
-            button.innerHTML = 'Export';
-            await assistOS.UI.closeModal(this.element);
+            this.taskId = await documentModule.exportDocument(assistOS.space.id, this.documentId, this.exportType);
+            await NotificationRouter.subscribeToSpace(assistOS.space.id, this.taskId, this.boundsOnCompleteExport);
         } catch (e){
             button.classList.remove('loading-icon');
             button.innerHTML = 'Export';
