703cb39a6c11f975d03efd116b19ad8bcdbcebed
alupoaiemircea <mircea@axiologic.net>
2024-12-04 10:16:20 +0200

    fix export document

--- apihub-components/tasks/ExportDocument.js
+++ apihub-components/tasks/ExportDocument.js
@@ -61,25 +61,36 @@ class ExportDocument extends Task {
             };
             archive.append(contentBuffer, {name: 'data.json'});
             archive.append(Buffer.from(JSON.stringify(metadata), 'utf-8'), {name: 'metadata.json'});
-            for(let personalityId of documentData.personalities){
-                const personalityStream = await space.APIs.archivePersonality(this.spaceId, personalityId);
-                archive.append(personalityStream, {name: `personalities/${personalityId}.persai`});
+            try {
+                for(let personalityId of documentData.personalities){
+                    const personalityStream = await space.APIs.archivePersonality(this.spaceId, personalityId);
+                    archive.append(personalityStream, {name: `personalities/${personalityId}.persai`});
+                }
+                await this.appendFilesInBatches(archive, documentData.images, Storage.fileTypes.images);
+                await this.appendFilesInBatches(archive, documentData.audios, Storage.fileTypes.audios);
+                await this.appendFilesInBatches(archive, documentData.videos, Storage.fileTypes.videos);
+                await archive.finalize();
+            } catch (e) {
+                reject(e);
             }
-
-            await this.appendFilesInBatches(archive, documentData.images, Storage.fileTypes.images);
-            await this.appendFilesInBatches(archive, documentData.audios, Storage.fileTypes.audios);
-            await this.appendFilesInBatches(archive, documentData.videos, Storage.fileTypes.videos);
-
-            await archive.finalize();
         });
     }
     async appendFilesInBatches(archive, fileDataList, fileType, batchSize = 10) {
         for (let i = 0; i < fileDataList.length; i += batchSize) {
+            //console.log(`processing ${fileType} ${i} to ${i + batchSize}`);
             const batch = fileDataList.slice(i, i + batchSize);
-            await Promise.all(batch.map(async (data) => {
-                const {fileStream, headers} = await Storage.getFile(fileType, data.id);
-                archive.append(fileStream, {name: `${fileType}/${data.name}.${fileType === Storage.fileTypes.images ? 'png' : fileType === Storage.fileTypes.audios ? 'mp3' : 'mp4'}`});
-            }));
+            try {
+                await Promise.all(batch.map(async (data) => {
+                    const {fileStream, headers} = await Storage.getFile(fileType, data.id);
+                    await new Promise((resolve, reject) => {
+                        archive.append(fileStream, {name: `${fileType}/${data.name}.${fileType === Storage.fileTypes.images ? 'png' : fileType === Storage.fileTypes.audios ? 'mp3' : 'mp4'}`});
+                        fileStream.on('end', resolve);
+                        fileStream.on('error', reject);
+                    });
+                }));
+            } catch (e) {
+                throw new Error(`Batch ${i + batchSize} -> ${batch.length} failed. file type: ${fileType} : ${e.message}`);
+            }
         }
     }
     async exportDocumentDataPartially(document) {
