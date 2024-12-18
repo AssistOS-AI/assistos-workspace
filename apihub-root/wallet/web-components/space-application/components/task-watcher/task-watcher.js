d1b814ed3a5f8e5a20bd1eed35567a311341af82
VictorCondurat <conduratvictor@yahoo.com>
2024-12-18 15:01:58 +0200

    feat: use log-entry component, add debouncing, and maxLogs

--- apihub-root/wallet/web-components/space-application/components/task-watcher/task-watcher.js
+++ apihub-root/wallet/web-components/space-application/components/task-watcher/task-watcher.js
@@ -7,37 +7,79 @@ export class TaskWatcher {
         this.taskId = this.element.getAttribute('taskId');
         this.boundOnTaskLog = this.onTaskLog.bind(this);
         this.historyLogQueue = [];
-        this.invalidate();
+        this.logBuffer = [];
+        this.debouncedProcessLogBuffer =debounce(this.processLogBuffer.bind(this), 200);
+        this.renderedLogHistory = false;
         this.monitorPresenter = this.element.closest('notifications-monitor').webSkelPresenter;
+        this.invalidate();
     }
 
-
     async beforeRender() {
-
     }
 
     async afterRender() {
         this.logBox = this.element.querySelector('#logViewerContent');
         this.loadingSpinner = this.element.querySelector('#loadingSpinner');
-        await assistOS.NotificationRouter.subscribeToSpace(assistOS.space.id, this.taskId + "/logs", this.boundOnTaskLog);
+        await assistOS.NotificationRouter.subscribeToSpace(
+            assistOS.space.id,
+            `${this.taskId}/logs`,
+            this.boundOnTaskLog
+        );
         this.renderedLogHistory = true;
+
+        if (this.historyLogQueue.length > 0) {
+            await this.processLogs(this.historyLogQueue);
+            this.historyLogQueue = [];
+        }
     }
 
     async onTaskLog(logData) {
         if (!this.renderedLogHistory) {
             this.historyLogQueue.push(logData);
-        }else{
-            if(this.historyLogQueue.length>0){
-                while(this.historyLogQueue.length>0){
-                    await this.processLog(this.historyLogQueue.shift());
-                }
-            }
-            await this.processLog(logData);
+        } else {
+            this.logBuffer.push(logData);
+            this.debouncedProcessLogBuffer();
         }
     }
 
-    async processLog(logData){
-        const {logType, message, data} = logData;
+    async processLogs(logs) {
+        for (const log of logs) {
+            await this.processLog(log);
+        }
+    }
+
+    processLogBuffer() {
+        if (this.logBuffer.length === 0) return;
+
+        const fragment = document.createDocumentFragment();
+
+        this.logBuffer.forEach(logData => {
+            const { logType, message, data, time } = logData;
+            const logEntry = document.createElement('log-entry');
+            logEntry.setAttribute('data-presenter', 'log-entry');
+            logEntry.logType= logType;
+            logEntry.message=message;
+            logEntry.time=time;
+            logEntry.dataSet={};
+            if (data) {
+                Object.keys(data).forEach(key => {
+                    logEntry.dataSet[key] = data[key];
+                });
+            }
+            fragment.appendChild(logEntry);
+        });
+
+        this.logBox.insertBefore(fragment, this.loadingSpinner);
+        this.logBuffer = [];
+
+        const maxLogs = 150;
+        while (this.logBox.children.length > maxLogs) {
+            this.logBox.removeChild(this.logBox.firstChild);
+        }
+    }
+
+    async processLog(logData) {
+        const { data } = logData;
 
         if (data?.finished) {
             if (this.loadingSpinner) {
@@ -50,36 +92,13 @@ export class TaskWatcher {
         if (data?.taskId) {
             this.monitorPresenter.addTaskWatcher(data.taskId);
         }
-        this.addLogEntry(message, logType);
+        this.addLogEntry(logData);
     }
 
-    addLogEntry(message, logType) {
-        if (message) {
-            const logEntry = document.createElement('div');
-            logEntry.className = 'logEntry';
-            switch (logType) {
-                case 'ERROR':
-                    logEntry.classList.add('logError');
-                    break;
-                case 'WARNING':
-                    logEntry.classList.add('logWarning');
-                    break;
-                case 'INFO':
-                    logEntry.classList.add('logInfo');
-                    break;
-                case 'SUCCESS':
-                    logEntry.classList.add('logSuccess');
-                    break;
-                case 'PROGRESS':
-                    logEntry.classList.add('logProgress');
-                    break;
-                default:
-                    logEntry.classList.add('logDefault');
-                    break;
-            }
-
-            logEntry.textContent = message;
-            this.logBox.insertBefore(logEntry, this.loadingSpinner);
+    addLogEntry(logData) {
+        if (logData.message) {
+            this.logBuffer.push(logData);
+            this.debouncedProcessLogBuffer();
         }
     }
 
@@ -94,4 +113,14 @@ export class TaskWatcher {
         await assistOS.NotificationRouter.unsubscribeFromObject(`${assistOS.space.id}/${this.taskId}/logs`);
         this.element.remove();
     }
+}
+
+function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
 }
