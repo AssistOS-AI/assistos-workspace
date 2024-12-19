42f5219972aa3416eb9302aacc42946024b0cd70
VictorCondurat <conduratvictor@yahoo.com>
2024-12-19 14:33:24 +0200

    chore: quickfix

--- apihub-components/apihub-component-utils/lightDB.js
+++ apihub-components/apihub-component-utils/lightDB.js
@@ -110,13 +110,11 @@ async function getContainerObjectsMetadata(spaceId, objectType) {
     let errorFlowObject = {};
     let errorDeleteObject = {};
     try {
-        await deleteRecord(spaceId, objectType, "documents_4fHrmFbM7TrhBqbc");
-        await deleteTable(spaceId, "documents_4fHrmFbM7TrhBqbc");
-      /*  await deleteRecord(spaceId, objectType, "documents_4TeMPRqY6phrQcFA");
-        await deleteTable(spaceId, "documents_4TeMPRqY6phrQcFA");
-        await deleteRecord(spaceId, objectType, "documents_4K1mYPwMoQdXXDuz");
-        await deleteTable(spaceId, "documents_4K1mYPwMoQdXXDuz");*/
-        console.log(`--------------------Deleted documents_4fHrmFbM7TrhBqbc`)
+        await deleteRecord(spaceId, objectType, "documents_4fLgGc5WdVDyBHWE");
+        await deleteTable(spaceId, "documents_4fLgGc5WdVDyBHWE");
+        await deleteRecord(spaceId, objectType, "documents_4af1Y4F7QDxNSWb4");
+        await deleteTable(spaceId, "documents_4af1Y4F7QDxNSWb4");
+
     } catch (error) {
         errorDeleteObject.error = error;
         errorDeleteObject.errorMessage = error.message
