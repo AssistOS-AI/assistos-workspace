95c3b4440d22df47aa4d9f964225e8e1b3a35b56
VictorCondurat <conduratvictor@yahoo.com>
2025-01-28 11:18:38 +0200

    feat: Remove API Key field from create space modal

--- apihub-root/wallet/web-components/space-application/modals/add-space-modal/add-space-modal.js
+++ apihub-root/wallet/web-components/space-application/modals/add-space-modal/add-space-modal.js
@@ -18,21 +18,9 @@ export class AddSpaceModal {
     async addSpace(_target) {
         let formData = await assistOS.UI.extractFormInformation(_target);
         if (formData.isValid) {
-
-            /* TODO It does not make sense to use an editable flow for default Application operations
-            *   Hard to debug, prone to errors, hard to separate the UI logic with data logic (when to refresh, when to close the modal, etc)
-            * */
-
-            /*
-             await assistOS.callFlow("AddSpace", {
-                 name: formData.data.name,
-                 apiKey: formData.data.spaceAPIKey
-             });
-             */
-            const [spaceName,apiKey]=[formData.data.name,formData.data.spaceAPIKey]
-
+            const spaceName=formData.data.name;
             try {
-                await assistOS.createSpace(spaceName,apiKey);
+                await assistOS.createSpace(spaceName);
                 assistOS.UI.closeModal(_target);
 
             } catch (error) {
