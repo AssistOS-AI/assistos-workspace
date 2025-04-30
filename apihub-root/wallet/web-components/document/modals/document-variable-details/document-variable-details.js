a9b2f4c4b7353d59fa75bd56a2ea92a0ac3edaab
alupoaiemircea <mircea@axiologic.net>
2025-04-30 15:40:54 +0300

    Refactor variable edit modals and improve UI consistency

---
+++ apihub-root/wallet/web-components/document/modals/document-variable-details/document-variable-details.js
@@ -0,0 +1,37 @@
+export class DocumentVariableDetails {
+    constructor(element, invalidate) {
+        this.element = element;
+        this.invalidate = invalidate;
+        this.documentId = this.element.getAttribute("data-document-id");
+        let documentPresenter = document.querySelector("document-view-page").webSkelPresenter;
+        this.document = documentPresenter._document;
+        this.element.classList.add("maintain-focus");
+        this.varName = this.element.getAttribute("data-name");
+        this.encodedExpression = this.element.getAttribute("data-expression");
+        this.activeTab = "valuesTab";
+        this.invalidate();
+    }
+
+    async beforeRender() {
+        if (this.activeTab === "valuesTab") {
+            this.tabContent = `<variable-values-tab data-name="${this.varName}" data-presenter="variable-values-tab"></variable-values-tab>`;
+        } else if (this.activeTab === "editTab") {
+            this.tabContent = `<edit-variable-tab data-name="${this.varName}" data-expression="${this.encodedExpression}" data-presenter="edit-variable-tab"></edit-variable-tab>`;
+        }
+    }
+
+    afterRender() {
+        let activeTab = this.element.querySelector(`.${this.activeTab}`);
+        activeTab.classList.add("active");
+        let icon = activeTab.querySelector(".tab-icon");
+        icon.style.setProperty('--icon-color', 'var(--blue-button)');
+    }
+
+    changeTab(_eventTarget, tabName) {
+        this.activeTab = tabName;
+        this.invalidate();
+    }
+    closeModal(){
+         assistOS.UI.closeModal(this.element)
+    }
+}
