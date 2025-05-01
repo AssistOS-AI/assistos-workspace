01d8b45f1f4282e7cd42235913247ef69f38ced5
alupoaiemircea <mircea@axiologic.net>
2025-05-01 11:23:15 +0300

    Update authentication method parameter to "emailCode"
    Replaced "email" with "emailCode" in API calls for generating auth codes and logging in. This ensures consistency with the updated authentication parameter specifications.

--- apihub-root/wallet/web-components/space-application/pages/authentication-page/authentication-page.js
+++ apihub-root/wallet/web-components/space-application/pages/authentication-page/authentication-page.js
@@ -147,7 +147,7 @@ export class AuthenticationPage {
         let result = await userModule.userExists(email);
         await assistOS.UI.hideLoading(loader);
         if(result.userExists){
-            result = await userModule.generateAuthCode(email, "email");
+            result = await userModule.generateAuthCode(email, "emailCode");
             this.email = email;
         } else {
             let signUpMessage = `We couldn't find an account for ${email}. Do you want to create a new account with this email?`;
@@ -155,7 +155,7 @@ export class AuthenticationPage {
                 message: signUpMessage
             }, true);
             if (signUpConfirmation) {
-                result = await userModule.generateAuthCode(email, "email");
+                result = await userModule.generateAuthCode(email, "emailCode");
                 this.email = email;
                 this.createSpace = true;
             } else {
@@ -169,7 +169,7 @@ export class AuthenticationPage {
     }
     async submitCode(_target) {
         let authCode = this.element.querySelector("#authCode").value;
-        let result = await userModule.loginUser(this.email, authCode, "email");
+        let result = await userModule.loginUser(this.email, authCode, "emailCode");
         if(result.operation !== "success"){
             throw new Error(result.message);
         }
