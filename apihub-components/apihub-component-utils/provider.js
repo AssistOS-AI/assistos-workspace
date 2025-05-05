4fc33380a0e471b676ac30607d9b7049072c6b76
VictorCondurat <conduratvictor@yahoo.com>
2025-05-05 15:54:06 +0300

    refactor(provider): update to ES6 module syntax and improve argument handling

--- apihub-components/apihub-component-utils/provider.js
+++ apihub-components/apihub-component-utils/provider.js
@@ -1,33 +1,35 @@
-const Binaries = require("./binaries");
-const path = require("path");
-const composeBinaryPath = (binary) => path.resolve(process.env.PERSISTENCE_FOLDER, `../binaries/${binary}.js`);
+import Binaries from './binaries.js'
+import path from 'path'
 
-const buildArgs = function ({
-                                subcommand,
-                                apiKey,
-                                model,
-                                promptOrMessages,
-                                options = {},
-                                streaming = false
-                            }) {
-    const args = [subcommand, "-k", apiKey, "-m", model];
+const composeBinaryPath = binary =>
+    path.resolve(process.env.PERSISTENCE_FOLDER, `../binaries/${binary}.js`)
+
+const buildArgs = ({
+                       subcommand,
+                       apiKey,
+                       model,
+                       promptOrMessages,
+                       options = {},
+                       streaming = false,
+                   }) => {
+    const args = [subcommand, '-k', apiKey, '-m', model]
 
     if (typeof promptOrMessages === 'string') {
-        args.push("-p", promptOrMessages);
+        args.push('-p', promptOrMessages)
     } else {
-        args.push("-p", JSON.stringify(promptOrMessages));
+        args.push('-p', JSON.stringify(promptOrMessages))
     }
 
-    if (streaming) args.push("--stream");
+    if (streaming) args.push('--stream')
 
-    if (options.temperature !== undefined) args.push("--temperature", options.temperature)
-    if (options.top_p !== undefined) args.push("--top_p", options.top_p)
-    if (options.frequency_penalty !== undefined) args.push("--frequency_penalty", options.frequency_penalty)
-    if (options.presence_penalty !== undefined) args.push("--presence_penalty", options.presence_penalty)
-    if (options.stop !== undefined) args.push("--stop", options.stop)
-    if (options.max_tokens !== undefined) args.push("--max_tokens", options.max_tokens)
+    if (options.temperature !== undefined) args.push('--temperature', options.temperature)
+    if (options.top_p !== undefined) args.push('--top_p', options.top_p)
+    if (options.frequency_penalty !== undefined) args.push('--frequency_penalty', options.frequency_penalty)
+    if (options.presence_penalty !== undefined) args.push('--presence_penalty', options.presence_penalty)
+    if (options.stop !== undefined) args.push('--stop', options.stop)
+    if (options.max_tokens !== undefined) args.push('--max_tokens', options.max_tokens)
 
-    return args;
+    return args
 }
 
 class Provider {
@@ -41,63 +43,44 @@ class Provider {
     }
 
     async getTextResponse(model, prompt, options = {}) {
-        if (!this.models[model]) {
-            throw new Error(`Provider ${this.name} does not support model ${model}`);
-        }
-        const args = buildArgs({
-            subcommand: "generateText",
-            model,
-            promptOrMessages: prompt,
-            options
-        });
-        let binariesPath = composeBinaryPath(this.name);
-        return await Binaries.executeBinary(this.name, binariesPath, args);
+        if (!this.models[model]) throw new Error(`Provider ${this.name} does not support model ${model}`)
+        const args = buildArgs({ subcommand: 'generateText', model, promptOrMessages: prompt, options })
+        const binariesPath = composeBinaryPath(this.name)
+        return await Binaries.executeBinary(this.name, binariesPath, args)
     }
 
     async getTextStreamingResponse(model, prompt, options = {}, onDataChunk) {
-        if (!this.models[model]) {
-            throw new Error(`Provider ${this.name} does not support model ${model}`);
-        }
+        if (!this.models[model]) throw new Error(`Provider ${this.name} does not support model ${model}`)
         const args = buildArgs({
-            subcommand: "generateTextStreaming",
+            subcommand: 'generateTextStreaming',
             model,
             promptOrMessages: prompt,
             options,
-            streaming: true
-        });
-        let binariesPath = composeBinaryPath(this.name);
-        return await Binaries.executeBinaryStreaming(this.name, binariesPath, args, onDataChunk);
+            streaming: true,
+        })
+        const binariesPath = composeBinaryPath(this.name)
+        return await Binaries.executeBinaryStreaming(this.name, binariesPath, args, onDataChunk)
     }
 
     async getChatCompletionResponse(model, messages, options = {}) {
-        if (!this.models[model]) {
-            throw new Error(`Provider ${this.name} does not support model ${model}`);
-        }
-        const args = buildArgs({
-            subcommand: "getChatCompletion",
-            model,
-            promptOrMessages: messages,
-            options
-        });
-        let binariesPath = composeBinaryPath(this.name);
-        return await Binaries.executeBinary(this.name, binariesPath, args);
+        if (!this.models[model]) throw new Error(`Provider ${this.name} does not support model ${model}`)
+        const args = buildArgs({ subcommand: 'getChatCompletion', model, promptOrMessages: messages, options })
+        const binariesPath = composeBinaryPath(this.name)
+        return await Binaries.executeBinary(this.name, binariesPath, args)
     }
 
     async getChatCompletionStreamingResponse(model, messages, options = {}, onDataChunk) {
-        if (!this.models[model]) {
-            throw new Error(`Provider ${this.name} does not support model ${model}`);
-        }
+        if (!this.models[model]) throw new Error(`Provider ${this.name} does not support model ${model}`)
         const args = buildArgs({
-            subcommand: "getChatCompletionStreaming",
+            subcommand: 'getChatCompletionStreaming',
             model,
-            prompt: messages,
+            promptOrMessages: messages,
             options,
-            streaming: true
-        });
-        const binariesPath = composeBinaryPath(this.name);
-        return await Binaries.executeBinaryStreaming(this.name, binariesPath, args, onDataChunk);
+            streaming: true,
+        })
+        const binariesPath = composeBinaryPath(this.name)
+        return await Binaries.executeBinaryStreaming(this.name, binariesPath, args, onDataChunk)
     }
-
 }
 
-module.exports = Provider;
+export default Provider
