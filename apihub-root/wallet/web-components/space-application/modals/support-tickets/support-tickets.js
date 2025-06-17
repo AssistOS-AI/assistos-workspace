ba308b3d5f712f71cda223e2600cdeca5bc3d0a3
alupoaiemircea <mircea@axiologic.net>
2025-06-17 11:08:18 +0300

    feat: enhance support ticket functionality; add ticket details view, resolution feature, and pagination improvements

--- apihub-root/wallet/web-components/space-application/modals/support-tickets/support-tickets.js
+++ apihub-root/wallet/web-components/space-application/modals/support-tickets/support-tickets.js
@@ -8,37 +8,45 @@ export class SupportTickets {
     }
     async beforeRender() {
         this.offset = 0;
-        this.paginationLimit = 11;
+        this.paginationLimit = 9;
         this.tickets = await userModule.getTickets(this.offset, this.paginationLimit);
-        let ticketsHTML = "";
-        for(let ticket of this.tickets) {
-            ticketsHTML += `
-            <div class="ticket">
-                <div class="ticket-preview">
-                     <div class="email">Submitted by: ${ticket.email}</div>
-                     <div class="email">Subject: ${ticket.subject}</div>
-                     <div data-local-action="openTicket">View</div>
-                </div>
-            </div>`;
-        }
-        if (this.tickets.length === 0) {
-            ticketsHTML = `<div class="no-tickets">No tickets submitted</div>`;
-        }
-        this.ticketsHTML = ticketsHTML;
-    }
-    openTicket(button, id){
-        let ticket = this.tickets.find(ticket => ticket.id === id);
-        let ticketPreview = button.closest('.ticket-preview');
-        let ticketDetails = `
-                <div class="ticket-details">
-                    <div class="message">Message: ${ticket.message}</div>
-                    <textarea class="form-input"></textarea>
-                    <div class="general-button">Mark as resolved</div>
-                </div>`;
-
+        this.ticketsHTML = this.getTicketsHTML();
     }
     afterRender() {
         this.changePaginationArrows();
+    }
+    openTicket(button, id){
+        let ticketElement = button.closest('.ticket');
+        let ticketDetailsElement = ticketElement.querySelector('.ticket-details');
+        if(ticketDetailsElement) {
+            return;
+        }
+        let ticket = this.tickets.find(ticket => ticket.id === id);
+        let ticketDetails = `
+                <div class="ticket-details">
+                    <div class="ticket-header">
+                    Message:
+                        <img class="close-icon pointer" data-local-action="closeTicket" src="./wallet/assets/icons/x-mark.svg" alt="close">
+                    </div>
+                    <div class="message">${ticket.message}</div>
+                    <label class="form-label" for="resolutionMessage">Provide a resolution message:</label>
+                    <textarea class="form-input" name="resolutionMessage" id="resolutionMessage"></textarea>
+                    <div class="general-button mark-resolved" data-local-action="resolveTicket ${id}">Mark as resolved</div>
+                </div>`;
+        ticketElement.insertAdjacentHTML("beforeend", ticketDetails);
+
+    }
+    closeTicket(button) {
+        let ticketDetails = button.closest('.ticket-details');
+        ticketDetails.remove();
+    }
+
+    async resolveTicket(button, id){
+        let ticketElement= button.closest('.ticket');
+        let resolutionMessage = ticketElement.querySelector('#resolutionMessage').value;
+        await userModule.resolveTicket(id, resolutionMessage);
+        ticketElement.classList.add("resolved");
+        this.closeTicket(button);
     }
     changePaginationArrows(){
         let nextButton = this.element.querySelector('.next-tickets');
@@ -55,7 +63,48 @@ export class SupportTickets {
             prevButton.classList.remove('disabled');
         }
     }
-
+    async changePage(button, direction) {
+        if (direction === 'next') {
+            this.offset += this.paginationLimit - 1;
+        } else if (direction === 'previous') {
+            this.offset -= this.paginationLimit - 1;
+        }
+        let ticketsList = this.element.querySelector('.tickets');
+        await assistOS.loadifyComponent(ticketsList, async () => {
+            this.tickets = await userModule.getTickets(this.offset, this.paginationLimit);
+            this.displayTickets();
+            this.changePaginationArrows();
+        });
+    }
+    displayTickets(){
+        let ticketsContainer = this.element.querySelector('.tickets');
+        ticketsContainer.innerHTML = this.getTicketsHTML();
+    }
+    getTicketsHTML(){
+        let ticketsHTML = "";
+        if (this.tickets.length === 0) {
+            return `<div class="no-tickets">No tickets submitted</div>`;
+        }
+        for(let i= 0; i < this.paginationLimit - 1; i++){
+            let ticket = this.tickets[i];
+            if(!ticket) {
+                continue;
+            }
+            let resolvedClass = ticket.resolved ? 'resolved' : '';
+            ticketsHTML += `
+            <div class="ticket ${resolvedClass}">
+                <div class="ticket-preview">
+                     <div class="ticket-info">
+                        <div class="email">Subject: ${ticket.subject}</div>
+                        <div class="email">Submitted by: ${ticket.email}</div>
+                     </div>
+                     <div class="view-ticket" data-local-action="openTicket ${ticket.id}">View</div>
+                     <img class="resolved-icon pointer" src="./wallet/assets/icons/success.svg" alt="resolved">
+                </div>
+            </div>`;
+        }
+        return ticketsHTML;
+    }
     closeModal(_target) {
         assistOS.UI.closeModal(_target);
     }
