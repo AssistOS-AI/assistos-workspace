e9bc5b8e6d11784e0b5148a970f7dfd2c0ca9182
skutner <sava.dumitru.daniel@gmail.com>
2025-08-08 11:25:54 +0300

    Improves document conversion process
    Enhances the document conversion process by improving error handling and logging in the DocsConverter plugin.
    
    - Adds more detailed logging for debugging purposes.
    - Improves error handling when calling the converter server,
      including logging the response body and attempting to parse
      the response as JSON.
    - Now returns the parsed JSON data directly from the converter
      server, rather than just the text content.

--- apihub-components/apihub-component-middlewares/context.js
+++ apihub-components/apihub-component-middlewares/context.js
@@ -1,6 +1,7 @@
 const cookie = require('../apihub-component-utils/cookie.js');
 async function contextMiddleware(req, res, next) {
     const cookies = cookie.parseRequestCookies(req);
+    
     req.currentSpaceId = cookies['currentSpaceId'];
     req.sessionId = cookies['sessionId'];
     req.userId = cookies['userId'];
