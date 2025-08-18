4b59dbe1354986ffec4587f9597c6fd60ab8560b
adiganga2002 <38322305+adiganga2002@users.noreply.github.com>
2025-08-18 10:39:52 +0300

    Add CodeManager plugin support to assistOS SDK
    Introduced a new plugin, CodeManager, to enhance code management capabilities. Updated constants, routing, and workspace plugins to handle CodeManager operations, including saving and retrieving code and web components. Added a dedicated module for the CodeManager plugin, facilitating seamless integration within the assistOS SDK.

--- apihub-components/globalServerlessAPI/workspacePlugins/CodeManager.js
+++ apihub-components/globalServerlessAPI/workspacePlugins/CodeManager.js
@@ -1,15 +1,38 @@
 const path = require("path");
 const fsPromises = require("fs/promises");
+
 async function WebAssistant() {
     const self = {};
 
-    self.getCodePath = function(fileName){
+    self.getCodePath = function (fileName) {
         return path.join(process.env.SERVERLESS_ROOT_FOLDER, "vibe-code", fileName);
     }
-    self.saveCode = async function(fileName, code){
+    self.saveCode = async function (fileName, code) {
+        const dir = path.dirname(self.getCodePath(fileName));
+        try {
+            await fsPromises.access(dir);
+        } catch (e) {
+            await fsPromises.mkdir(dir, {recursive: true});
+        }
         await fsPromises.writeFile(self.getCodePath(fileName), code);
     }
-    self.getCode = async function(fileName){
+
+    self.saveWebskelComponent = async function (fileName, componentData) {
+        let items = Object.keys(componentData);
+        if (!items || items.length === 0 || !componentData.html || !componentData.css) {
+            throw new Error("Invalid component data");
+        }
+        try {
+            for (let componentItem of items) {
+                await self.saveCode(`${fileName}.${componentItem}`, componentData[componentItem]);
+                return true;
+            }
+        } catch (e) {
+            return false;
+        }
+    }
+
+    self.getCode = async function (fileName) {
         return await fsPromises.readFile(self.getCodePath(fileName), "utf-8");
     }
 
@@ -19,6 +42,7 @@ async function WebAssistant() {
 
     return self;
 }
+
 let singletonInstance;
 
 module.exports = {
